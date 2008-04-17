af8ce339942beed04c16890fa2cadb88a4ca25df
shasti <shasti@945d5b45-6d3a-0410-8913-86594e9677e0>
2008-04-17 02:58:10 +0000

    calendar implementations

--- jquantlib/src/main/java/org/jquantlib/time/calendars/DelegateCalendar.java
+++ jquantlib/src/main/java/org/jquantlib/time/calendars/DelegateCalendar.java
@@ -24,26 +24,6 @@ import org.jquantlib.time.Calendar;
 import org.jquantlib.time.Period;
 import org.jquantlib.time.TimeUnit;
 import org.jquantlib.time.Weekday;
-
-/*
- * Copyright (C) 2008 Srinivas Hasti
- * 
- * This file is part of JQuantLib, a free-software/open-source library for
- * financial quantitative analysts and developers - http://jquantlib.org/
- * 
- * JQuantLib is free software: you can redistribute it and/or modify it under
- * the terms of the QuantLib license. You should have received a copy of the
- * license along with this program; if not, please email
- * <jquantlib-dev@lists.sf.net>. The license is also available online at
- * <http://jquantlib.org/license.shtml>.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the license for more details.
- * 
- * JQuantLib is based on QuantLib. http://quantlib.org/ When applicable, the
- * originating copyright notice follows below.
- */
 import org.jquantlib.util.Date;
 
 /**
