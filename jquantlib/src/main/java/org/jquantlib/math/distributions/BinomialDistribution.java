e5a52f82c16c2db2af691d3f101c2916f2fc1829
holdom <holdom@945d5b45-6d3a-0410-8913-86594e9677e0>
2008-05-06 07:10:00 +0000

    /math/distributions: Added C++ (commented out) and started to translate the missing methods. Not completed yet.

--- jquantlib/src/main/java/org/jquantlib/math/distributions/BinomialDistribution.java
+++ jquantlib/src/main/java/org/jquantlib/math/distributions/BinomialDistribution.java
@@ -1,10 +1,38 @@
+/*
+ Copyright (C) 2008 Richard Gomes
+
+ This file is part of JQuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://jquantlib.org/
+
+ JQuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <jquantlib-dev@lists.sf.net>. The license is also available online at
+ <http://jquantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+ 
+ JQuantLib is based on QuantLib. http://quantlib.org/
+ When applicable, the originating copyright notice follows below.
+ */
+
 package org.jquantlib.math.distributions;
 
 import org.jquantlib.math.Factorial;
+// import cern.jet.math.Functions; --> not yet implemented
 
 /**
- * @author <Richard Gomes>
+ * @author Richard Gomes
+ * @author Dominik Holenstein
  */
+
+
+//TODO: Translate C++ code (commented out) -> Dominik
+//TODO: Write tests for the new functions -> Dominik
+//TODO: Use Colt -> Dominik
+
 public class BinomialDistribution {
 
 	public BinomialDistribution(double p, int n) {
@@ -62,6 +90,80 @@ public class BinomialDistribution {
         return factorial_.ln(n)-factorial_.ln(k)-factorial_.ln(n-k);
 
     }
+	
+	/*
+	 inline Real binomialCoefficient(BigNatural n, BigNatural k) {
+        return std::floor(0.5+std::exp(binomialCoefficientLn(n, k)));
+    }
+	 */
+	
+	static private double binomialCoefficient(int n, int k) {
+		return Math.floor(0.5 + Math.exp(binomialCoefficientLn(n,k)));
+	}
+	
+	
+	//TODO: Translate C++ code (commented out) -> Dominik
+	//TODO: Write tests for the new functions -> Dominik
+	//TODO: Use Colt -> Dominik
+	
+	//! Cumulative binomial distribution function
+    /*! Given an integer k it provides the cumulative probability
+        of observing kk<=k:
+        formula here ...
+
+    */
+	
+	/*
+    class CumulativeBinomialDistribution
+    : public std::unary_function<Real,Real> {
+      public:
+        CumulativeBinomialDistribution(Real p, BigNatural n);
+        // function
+        Real operator()(BigNatural k) const {
+            if (k >= n_)
+                return 1.0;
+            else
+                return 1.0 - incompleteBetaFunction(k+1, n_-k, p_);
+        }
+      private:
+        BigNatural n_;
+        Real p_;
+    };
+    */
+	
+	/*
+	 * inline
+    CumulativeBinomialDistribution::CumulativeBinomialDistribution(
+                                                       Real p, BigNatural n)
+    : n_(n), p_(p) {
+
+        QL_REQUIRE(p>=0, "negative p not allowed");
+        QL_REQUIRE(p<=1.0, "p>1.0 not allowed");
+
+    } 
+    */
+	
+	
+	/*
+	 * /*! Given an odd integer n and a real number z it returns p such that:
+        1 - CumulativeBinomialDistribution((n-1)/2, n, p) =
+                               CumulativeNormalDistribution(z)
+
+        \pre n must be odd
+    
+    inline Real PeizerPrattMethod2Inversion(Real z, BigNatural n) {
+
+        QL_REQUIRE(n%2==1,
+                   "n must be an odd number: " << n << " not allowed");
+
+        Real result = (z/(n+1.0/3.0+0.1/(n+1.0)));
+        result *= result;
+        result = std::exp(-result*(n+1.0/6.0));
+        result = 0.5 + (z>0 ? 1 : -1) * std::sqrt((0.25 * (1.0-result)));
+        return result;
+    }
+	 */
+	
 	
 	private static final Factorial factorial_ = new Factorial();
     private int n_;
