7ad3e0b592be4955f132424dd84742cd00f11de3
holdom <holdom@945d5b45-6d3a-0410-8913-86594e9677e0>
2008-05-08 13:37:23 +0000

    /math/distributions: GammaFunction.java -> Translated the GammaFunction. Issue: How or where is the value a defined?
    /testsuite/math/distributions: Added the BinomialDistributionTest.java test case.

--- jquantlib/src/main/java/org/jquantlib/math/distributions/GammaFunction.java
+++ jquantlib/src/main/java/org/jquantlib/math/distributions/GammaFunction.java
@@ -1,12 +1,43 @@
+/*
+ Copyright (C) 2008 Richard Gomes
+
+ This file is part of JQuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://jquantlib.org/
+
+ JQuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <jquantlib-dev@lists.sf.net>. The license is also available online at
+ <http://jquantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+ 
+ JQuantLib is based on QuantLib. http://quantlib.org/
+ When applicable, the originating copyright notice follows below.
+ */
+
 package org.jquantlib.math.distributions;
 
 import org.jquantlib.math.UnaryFunctionDouble;
+import org.jquantlib.math.Constants;
 
 /**
- * @author <Richard Gomes>
+ * @author Richard Gomes
+ * @author Dominik Holenstein
  */
+
+
 public class GammaFunction implements UnaryFunctionDouble {
 
+	private static final double c1_ = 76.18009172947146;
+    private static final double c2_ = -86.50532032941677;
+    private static final double c3_ = 24.01409824083091;
+    private static final double c4_ = -1.231739572450155;
+    private static final double c5_ = 0.1208650973866179e-2;
+    private static final double c6_ = -0.5395239384953e-5;
+	
     public double logValue(double x) {
         if (!(x>0.0)){
         	throw new ArithmeticException("positive argument required " + x);
@@ -23,17 +54,61 @@ public class GammaFunction implements UnaryFunctionDouble {
 
         return -temp+Math.log(2.5066282746310005*ser/x);
     }
+    
+    public double evaluate(double x) {
+    	throw new UnsupportedOperationException() ;
+    }
+    
 
+    //FIXME: How is the value a obtained?
+    /*
 	public double evaluate(double x) {
-		throw new UnsupportedOperationException();
-	}
+		if (x <= 0.0) {
+			return 0.0;
+		}
 
-    private static final double c1_ = 76.18009172947146;
-    private static final double c2_ = -86.50532032941677;
-    private static final double c3_ = 24.01409824083091;
-    private static final double c4_ = -1.231739572450155;
-    private static final double c5_ = 0.1208650973866179e-2;
-    private static final double c6_ = -0.5395239384953e-5;
+        double gln = logValue(a);
+
+        if (x<(a+1.0)) {
+            double ap = a;
+            double del = 1.0/a;
+            double sum = del;
+            for (int n=1; n<=100; n++) {
+                ap += 1.0;
+                del *= x/ap;
+                sum += del;
+                if (Math.abs(del) < Math.abs(sum)*3.0e-7)
+                    return sum*Math.exp(-x + a*Math.log(x) - gln);
+            }
+        } else {
+            double b = x + 1.0 - a;
+            double c = Constants.QL_MAX_REAL;
+            double d = 1.0/b;
+            double h = d;
+            for (int n=1; n<=100; n++) {
+                double an = -1.0*n*(n-a);
+                b += 2.0;
+                d = an*d + b;
+                
+                if (Math.abs(d) < Constants.QL_EPSILON) {
+                	d = Constants.QL_EPSILON;
+                }
+                c = b + an/c;
+                if (Math.abs(c) < Constants.QL_EPSILON) {
+                	c = Constants.QL_EPSILON;
+                }
+                d = 1.0/d;
+                double del = d*c;
+                h *= del;
+                if (Math.abs(del - 1.0)<Constants.QL_EPSILON)
+                    return h*Math.exp(-x + a*Math.log(x) - gln);
+            }
+        }
+        throw new ArithmeticException("too few iterations");
+	}
+	*/
+
+    
 
 	
 }
--- jquantlib/src/test/java/org/jquantlib/testsuite/math/distributions/BinomialDistributionTest.java
+++ jquantlib/src/test/java/org/jquantlib/testsuite/math/distributions/BinomialDistributionTest.java
@@ -1,41 +1,74 @@
+/*
+ Copyright (C) 2008 Dominik Holenstein
+ 
+ This file is part of JQuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://jquantlib.org/
+
+ JQuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <jquantlib-dev@lists.sf.net>. The license is also available online at
+ <http://jquantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+ 
+ JQuantLib is based on QuantLib. http://quantlib.org/
+ When applicable, th
+
+*/
+
 package org.jquantlib.testsuite.math.distributions;
 
 import junit.framework.TestCase;
 
 import org.jquantlib.math.distributions.BinomialDistribution;
 import org.junit.Test;
-import org.junit.Ignore;
+// import org.junit.Ignore; --> for excluding from tests
+
+/**
+ * @author Dominik Holenstein
+ **/
 
 public class BinomialDistributionTest extends TestCase {
 	
-	// @Ignore ("under construction") @Test
-	
-	/*
+	@Test
 	public void testBinomialDistribution() {
+				
+		// Expected values with n = 16 trials and p = 0.5
+		double[]testvalues = {	1.52587890625E-5,
+								2.44140625E-4,
+								0.0018310546875,
+								0.008544921875,
+								0.02777099609375,
+								0.066650390625,
+								0.1221923828125,
+								0.174560546875,
+								0.19638061523438,
+								0.174560546875,
+								0.1221923828125,
+								0.066650390625,
+								0.02777099609375,
+								0.008544921875,
+								0.0018310546875,
+								2.44140625E-4,
+								}; 
 		
-		double[][] testvalues = {	{0.0, 0.398942280401433},
-									{1.0, 0.241970724519143},
-									{2.0, 0.053990966513188},
-									{3.0, 0.004431848411938},
-									{4.0, 1.338302258e-4},
-									{5.0, 1.486719515e-6}};
-		
-		double p = 0.5;
-		int n = 20;
+		double p = 0.5; 		   // p = probability
+		int n = testvalues.length; // n = number of trials
 		
 		BinomialDistribution binomdist = new BinomialDistribution(p,n);
 		
-		/*
-		// for(int i=0;i<testvalues.length;i++){
-			int z = n;
-			double expected = testvalues[i][1];
+		for(int i=0;i<n;i++){
+			int z = i;
+			double expected = testvalues[i];
 			double computed = binomdist.evaluate(z);
-			double tolerance = (Math.abs(z)<3.01) ? 1.0e-15: 1.0e-10;
-			assertEquals(expected, computed,tolerance);
-			assertEquals(expected, normal.evaluate(-z),tolerance);
-			
-		// }
-		 * 
-		 */
-
+			// System.out.println("computed: " + computed); // --> for testing while developing
+			// double tolerance = (z<6 ) ? 1.0e-15: 1.0e-10;
+			double tolerance = 1.0e-10;
+			// System.out.println("tolerance: " + tolerance); // --> for testing while developing
+			assertEquals(expected, computed, tolerance);			
+		}
+	}
 }
