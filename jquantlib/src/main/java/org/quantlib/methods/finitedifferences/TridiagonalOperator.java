0a67de1e03fa21f941fb20a63966cecca0ba297f
shasti <shasti@945d5b45-6d3a-0410-8913-86594e9677e0>
2008-05-08 03:11:09 +0000

    comments

---
+++ jquantlib/src/main/java/org/quantlib/methods/finitedifferences/TridiagonalOperator.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package org.quantlib.methods.finitedifferences;
+
+/**
+ * @author shasti
+ * 
+ */
+public class TridiagonalOperator {
+	protected final double lowerDiagnol[];
+	protected final double diagnol[];
+	protected final double upperDiagnol[];
+
+	public TridiagonalOperator(int size) {
+		if (size >= 2) {
+			this.lowerDiagnol = new double[size - 1];
+			this.diagnol = new double[size];
+			this.upperDiagnol = new double[size - 1];
+		} else if (size == 0) {
+			this.lowerDiagnol = new double[0];
+			this.diagnol = new double[0];
+			this.upperDiagnol = new double[0];
+		} else {
+			throw new IllegalStateException("Invalid size " + size);
+		}
+
+	}
+
+	public TridiagonalOperator(double[] ldiag, double[] diag, double[] udiag) {
+		if (ldiag.length != diag.length - 1)
+			throw new IllegalStateException("wrong size for lower diagnol");
+		if (udiag.length == diag.length - 1)
+			throw new IllegalStateException("wrong size for upper diagnol");
+		this.lowerDiagnol = ldiag;
+		this.diagnol = diag;
+		this.upperDiagnol = udiag;
+	}
+	
+	
+}
--- jquantlib/src/test/java/org/jquantlib/perf/math/BinomialTest.java
+++ jquantlib/src/test/java/org/jquantlib/perf/math/BinomialTest.java
@@ -1,3 +1,22 @@
+/*
+ Copyright (C) 2007 Srinivas Hasti
+
+ This file is part of JQuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://jquantlib.org/
+
+ JQuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <jquantlib-dev@lists.sf.net>. The license is also available online at
+ <http://jquantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+ 
+ JQuantLib is based on QuantLib. http://quantlib.org/
+ When applicable, the originating copyright notice follows below.
+ */
 package org.jquantlib.perf.math;
 
 import org.jquantlib.math.distributions.BinomialDistribution;
@@ -16,7 +35,7 @@ public class BinomialTest {
 	  for(int i=0;i<100;i++){
 		  clock.reset();
 		  clock.startClock();
-		  binomial.pdf(5);
+		  binomial.pdf(1900);
 		  clock.stopClock();
 	  }
 	  
