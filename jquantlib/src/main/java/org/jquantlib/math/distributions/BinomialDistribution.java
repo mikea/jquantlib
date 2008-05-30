c68f7feef9bac6a6a086fce679aa79ac345b2bf1
holdom <holdom@945d5b45-6d3a-0410-8913-86594e9677e0>
2008-05-30 14:10:33 +0000

    BinomialDistribution.java: Started to revise the code based on Checkstyle and to add JavaDoc.

--- jquantlib/src/main/java/org/jquantlib/math/distributions/BinomialDistribution.java
+++ jquantlib/src/main/java/org/jquantlib/math/distributions/BinomialDistribution.java
@@ -26,8 +26,8 @@ import org.jquantlib.math.Factorial;
  * @author Richard Gomes
  * @author Dominik Holenstein
  * <p>
- * 
- * @see 
+ *
+ * @see
  * <strong>Binomial Distribution</strong><br>
  * (source: <a href="http://en.wikipedia.org/wiki/Binomial_distribution">Binomial Distribution on Wikipedia</a>)<br>
  * In probability theory and statistics, the binomial distribution <br>
@@ -39,74 +39,102 @@ import org.jquantlib.math.Factorial;
  * distribution is the basis for the popular binomial test of statistical <br>
  * significance. A binomial distribution should not be confused with a <br>
  * bimodal distribution.<p>
- * 
+ *
  */
 
 public class BinomialDistribution {
-	
-	private static final Factorial factorial_ = new Factorial();
-    private int n_;
-    private double logP_, logOneMinusP_;
-    
 
-	public BinomialDistribution(double p, int n) {
+	private static final Factorial factorial_ = new Factorial();
+	
+	/**
+	 * @param n_ sequence of independent yes/no experiments
+	 */
+    private int n_;
+    
+    /**
+     * @param logP_ Natural logarithm of p
+     * @param logOneMinusP_ Natural logarithm of 1-p
+     */
+    private double logP_, logOneMinusP_;
+
+
+    /**
+     * <b>public BinomilalDistribution(final double p, final int n)</b> <br>
+     * Constructor to initialize the private p and n and to ensure that 
+     * p is within the range of [0..1]. <br>
+     * If p = 0.0 then logMinusP_ is set to 0.0. <br>
+     * If p = 1.0 then logP is set to 1.0. <p>
+     * 
+     * @param p	Probability
+     * @param n Sequence of independent yes/no experiments
+     */
+	public BinomialDistribution(final double p, final int n) {
 		n_=n;
 
-		if (p==0.0) {
+		if (p == 0.0) {
 			logOneMinusP_ = 0.0;
-		} else if (p==1.0) {
+		} else if (p == 1.0) {
 			logP_ = 0.0;
 		} else {
-			if (!(p>0)){
+			if (!(p > 0)){
 				throw new ArithmeticException("negative p not allowed");
 			}
-			if (!(p<1.0)){
+			if (!(p < 1.0)){
 				throw new ArithmeticException("p>1.0 not allowed");
 			}
-
+		
 			logP_ = Math.log(p);
-			logOneMinusP_ = Math.log(1.0-p);
-			
+			logOneMinusP_ = Math.log(1.0 - p);
+
 		}
 
 	}
 	
 	// TODO Consider developing an UnaryFunctionSomething for int vars.
-	public double evaluate(int k) {
+	public double evaluate(final int k) {
 
         if (k > n_) return 0.0;
 
-        // p==1.0
-        if (logP_==0.0) {
-            return (k==n_ ? 1.0 : 0.0);
+        // p == 1.0
+        if (logP_ == 0.0) {
+            return (k == n_ ? 1.0 : 0.0);
         }
         // p==0.0
-        if (logOneMinusP_==0.0){
-            return (k==0 ? 1.0 : 0.0);
+        if (logOneMinusP_ == 0.0){
+            return (k == 0 ? 1.0 : 0.0);
         }
-        
         return Math.exp(binomialCoefficientLn(n_, k) +
                             k * logP_ + (n_-k) * logOneMinusP_);
-
 	}
-	
-	static private double binomialCoefficientLn(int n, int k) {
 
-		if (!(n>=0)) {
+	/**
+	 * 
+	 * @param n
+	 * @param k
+	 * @return Natural logartihm of the binomial coefficient
+	 */
+	private static double binomialCoefficientLn(final int n, final int k) {
+
+		if (!(n >= 0)) {
 			throw new ArithmeticException("n<0 not allowed, " + n);
 		}
-		if (!(k>=0)) {
+		if (!(k >= 0)) {
 			throw new ArithmeticException("k<0 not allowed, " + k);
 		}
-		if (!(n>=k)){
+		if (!(n >= k)){
 			throw new ArithmeticException("n<k not allowed");
 		}
 
-        return factorial_.ln(n)-factorial_.ln(k)-factorial_.ln(n-k);
+        return factorial_.ln(n) - factorial_.ln(k) - factorial_.ln(n - k);
     }
 	
-	
-	static private double binomialCoefficient(int n, int k) {
-		return Math.floor(0.5 + Math.exp(binomialCoefficientLn(n,k)));
+	/**
+	 * 
+	 * @param n
+	 * @param k
+	 * @return Binomial Coefficient
+	 */
+	private static double binomialCoefficient(final int n, final int k) {
+		return Math.floor(0.5 + Math.exp(binomialCoefficientLn(n, k)));
 	}
 }
