c8a525371e93b196ee5c833328a4578c435fc2e9
holdom <holdom@945d5b45-6d3a-0410-8913-86594e9677e0>
2008-06-02 14:15:07 +0000

    JavaDoc added to CumulativePoisssonDistribution.java. Not finished yet.

--- jquantlib/src/main/java/org/jquantlib/math/distributions/CumulativePoissonDistribution.java
+++ jquantlib/src/main/java/org/jquantlib/math/distributions/CumulativePoissonDistribution.java
@@ -23,21 +23,29 @@ package org.jquantlib.math.distributions;
 
 
 /**
- * 
- * @author Dominik Holenstein
- * <p>
- * 
- * <strong>Cumulative Poisson distribution function</strong><br>
+ * Cumulative Poisson distribution function<br>
  * 
     This function provides an approximation of the
     integral of the Poisson distribution.<p>
+    
+    In probability theory and statistics, the Poisson <br>
+    distribution is a discrete probability distribution that <br>
+    expresses the probability of a number of events occurring in <br>
+    a fixed period of time if these events occur with a known <br>
+    average rate and independently of the time since the last <br>
+    event. The Poisson distribution can also be used for the <br>
+    number of events in other specified intervals such as distance, <br>
+    area or volume.<br>
+    (Source: <a href="http://en.wikipedia.org/wiki/Poisson_distribution">Poisson Distribution on Wikipedia</a>)<p>
 
      For this implementation see
      "Numerical Recipes in C", 2nd edition,
-     Press, Teukolsky, Vetterling, Flannery, chapter 6<p>
+     Press, Teukolsky, Vetterling, Flannery, chapter 6.<p>
 
      Test the correctness of the returned value is tested by
-     checking it against known good results
+     checking it against known good results.
+     
+     @author Dominik Holenstein
  *
  */
 
@@ -53,8 +61,16 @@ public class CumulativePoissonDistribution {
 			mu_ = mu;
 		}
         
+		/**
+		 * Computes the cumulative poisson distribution by using the 
+		 * incomplete gamma function.
+		 * @param k is the number of occurrences of an event 
+		 * @return 1.0 - incmplgamma.incompleteGammaFunction(k_+1, mu_, accuracy, maxIteration);
+		 */
 		// TODO Check double k_ = (double)k; -> is this cast a good idea?
        public  double evaluate (int k) /* @Read-only */ {
+    	   
+    	   	if (k < 0) throw new ArithmeticException("k must be >= 1, but is " + k);
         	
         	// this cast is necessary because the incompleteGammaFunction requires double,double,double,int parameters.
         	double k_ = (double)k;
