089840b1315a4f123593c3070cb144819b5d0964
holdom <holdom@945d5b45-6d3a-0410-8913-86594e9677e0>
2008-06-05 15:58:53 +0000

    Added JavaDocs to IncompleteGamma.

--- jquantlib/src/main/java/org/jquantlib/math/distributions/IncompleteGamma.java
+++ jquantlib/src/main/java/org/jquantlib/math/distributions/IncompleteGamma.java
@@ -29,79 +29,95 @@ import org.jquantlib.math.Constants;
 
 //FIXME: Check duplicated code compared to the GammaDistribution class
 public class IncompleteGamma {
-	
-	  public double incompleteGammaFunction(double a, double x, double accuracy, int maxIteration) {
-		  
-	      if (!(a > 0.0)) {
-		  throw new ArithmeticException("non-positive a is not allowed");
-	      }
-		 
-	      if(!(x>=0.0)){
-		  throw new ArithmeticException("negative x is not allowed");
-	      }
-		 
-	      if (x < (1+1.0)){
-		// Use the series representation
-		return incompleteGammaFunctionSeriesRepr(a,x,accuracy,maxIteration);
-	      }
-	      else {
-		// Use the continued fraction representation
-		return 1.0-incompleteGammaFunctionContinuedFractionRepr(a,x,accuracy,maxIteration);
-	      } 
-	  }
-	
-	double incompleteGammaFunctionSeriesRepr(double a, double x, double accuracy,int maxIteration) {
-	    if (x==0.0) {
-		return 0.0;
-	    }
-        
-	    GammaFunction gf = new GammaFunction();
-	    double gln = gf.logValue(a);
-	    double ap=a;
-	    double del=1.0/a;
-	    double sum=del;
-	    for (Integer n=1; n<=maxIteration; n++) {
-		++ap;
-		del *= x/ap;
-		sum += del;
-		if (Math.abs(del) < Math.abs(sum)*accuracy) {
-		    return sum*Math.abs(-x+a*Math.log(x)-gln);
-		}
-	    }
-	    throw new ArithmeticException("accuracy not reached");
+
+    public double incompleteGammaFunction(double a, double x, double accuracy, int maxIteration) {
+
+	if (!(a > 0.0)) {
+	    throw new ArithmeticException("non-positive a is not allowed");
 	}
 
-	public double incompleteGammaFunctionContinuedFractionRepr(double a, double x,
-                                                      double accuracy,
-                                                      int maxIteration) {
-
-	    int i;
-	    double an, b, c, d, del, h;
-        
-	    GammaFunction gf = new GammaFunction();
-	    double gln = gf.logValue(a);
-	    b=x+1.0-a;
-	    c=1.0/Constants.QL_EPSILON;
-	    d=1.0/b;
-	    h=d;
-	    for (i=1; i<=maxIteration; i++) {
-		an = -i*(i-a);
-		b += 2.0;
-		d=an*d+b;
-		if (Math.abs(d) < Constants.QL_EPSILON) {
-		    d=Constants.QL_EPSILON;
-		}
-		c=b+an/c;
-		if (Math.abs(c) < Constants.QL_EPSILON) {
-		    c=Constants.QL_EPSILON;
-		}
-		d=1.0/d;
-		del=d*c;
-		h *= del;
-		if (Math.abs(del-1.0) < accuracy) {
-		    return Math.exp(-x+a*Math.log(x)-gln)*h;
-		}
-	    }
-	    throw new ArithmeticException("accuracy not reached");
+	if (!(x >= 0.0)) {
+	    throw new ArithmeticException("negative x is not allowed");
 	}
+
+	if (x < (1 + 1.0)) {
+	    // Use the series representation
+	    return incompleteGammaFunctionSeriesRepr(a, x, accuracy, maxIteration);
+	} else {
+	    // Use the continued fraction representation
+	    return 1.0 - incompleteGammaFunctionContinuedFractionRepr(a, x, accuracy, maxIteration);
+	}
+    }
+
+    /**
+     * Computes the incomplete Gamma function by using the series
+     * representation.
+     * 
+     * @param a
+     * @param x
+     * @param accuracy
+     * @param maxIteration
+     * @return incomplete Gamma by using the series representation
+     */
+    double incompleteGammaFunctionSeriesRepr(double a, double x, double accuracy, int maxIteration) {
+	if (x == 0.0) {
+	    return 0.0;
+	}
+
+	GammaFunction gf = new GammaFunction();
+	double gln = gf.logValue(a);
+	double ap = a;
+	double del = 1.0 / a;
+	double sum = del;
+	for (Integer n = 1; n <= maxIteration; n++) {
+	    ++ap;
+	    del *= x / ap;
+	    sum += del;
+	    if (Math.abs(del) < Math.abs(sum) * accuracy) {
+		return sum * Math.abs(-x + a * Math.log(x) - gln);
+	    }
+	}
+	throw new ArithmeticException("accuracy not reached");
+    }
+
+    
+    /**
+     * Computes the incomplete Gamma function by using the continued fraction representation.
+     * @param a
+     * @param x
+     * @param accuracy
+     * @param maxIteration
+     * @return incomplete Gamma by using the continued fraction representation
+     */
+    public double incompleteGammaFunctionContinuedFractionRepr(double a, double x, double accuracy, int maxIteration) {
+
+	int i;
+	double an, b, c, d, del, h;
+
+	GammaFunction gf = new GammaFunction();
+	double gln = gf.logValue(a);
+	b = x + 1.0 - a;
+	c = 1.0 / Constants.QL_EPSILON;
+	d = 1.0 / b;
+	h = d;
+	for (i = 1; i <= maxIteration; i++) {
+	    an = -i * (i - a);
+	    b += 2.0;
+	    d = an * d + b;
+	    if (Math.abs(d) < Constants.QL_EPSILON) {
+		d = Constants.QL_EPSILON;
+	    }
+	    c = b + an / c;
+	    if (Math.abs(c) < Constants.QL_EPSILON) {
+		c = Constants.QL_EPSILON;
+	    }
+	    d = 1.0 / d;
+	    del = d * c;
+	    h *= del;
+	    if (Math.abs(del - 1.0) < accuracy) {
+		return Math.exp(-x + a * Math.log(x) - gln) * h;
+	    }
+	}
+	throw new ArithmeticException("accuracy not reached");
+    }
 }
