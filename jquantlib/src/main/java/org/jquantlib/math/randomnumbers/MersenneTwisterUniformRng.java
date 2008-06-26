8539ff883ee0319cb473e96b3e9d265880dcdc09
holdom <holdom@945d5b45-6d3a-0410-8913-86594e9677e0>
2008-06-26 14:22:53 +0000

    Worked on the MersenneTwisterUniformRng class. Still a very rough draft of the code and not working.

--- jquantlib/src/main/java/org/jquantlib/math/randomnumbers/MersenneTwisterUniformRng.java
+++ jquantlib/src/main/java/org/jquantlib/math/randomnumbers/MersenneTwisterUniformRng.java
@@ -20,10 +20,11 @@
  When applicable, the original copyright notice follows this notice.
  */
 
-
+/*
 package org.jquantlib.math.randomnumbers;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 // import org.jquantlib.math.randomnumbers.SeedGenerator;
 
@@ -38,6 +39,11 @@ import java.util.ArrayList;
 
 /*
 public class MersenneTwisterUniformRng {
+
+	private int mti; 
+	
+	// set initial seeds: N = 624 words
+	private int[] mt = new int[N];
 	
 	
 	//
@@ -45,58 +51,82 @@ public class MersenneTwisterUniformRng {
 	//
 	
 	// Period parameters
-	final static int N = 624;
-	final static int M = 397;
+	private static final int N = 624;
+	private static final int M = 397;
 
 	
 	// constant vector a	
-	final long MATRIX_A = 0x9908b0df;
+	private static final int MATRIX_A = 0x9908b0df;
 	
 	// most significant w-r bits
-	final long UPPER_MASK = 0x80000000;
+	private static final int UPPER_MASK = 0x80000000;
 	
 	// least significant r bits
-	final long LOWER_MASK = 0x7fffffff;
+	private static final int LOWER_MASK = 0x7fffffff;
+	
+	//
+	private static final int TEMPERING_MASK_B=0x9d2c5680;
+	private static final int TEMPERING_MASK_C=0xefc60000;
+	
+	public static final int DEFAULT_SEED = 4357;
 	
 	
 	//
-	// private fields
+	// Constructors
 	//
 	
-	// the array for the state vector
-	private ArrayList<Long> mt; 
+	/**
+	 * Constructs and returns a random number generator with a default seed, which is a <b>constant</b>.
+	 * Thus using this constructor will yield generators that always produce exactly the same sequence.
+	 * This method is mainly intended to ease testing and debugging.
+	 **/
+/*
+	public MersenneTwisterUniformRng() {
+		this(DEFAULT_SEED);
+	}
 	
-	// it is mutable in QuantLib
-	static int mti=N+1; // mti==N+1 means mt[N] is not initialized
+	/**
+	 * Constructs and returns a random number generator with the given seed.
+	 */
 	
-	
-	//
-	// Constructor
-	//
-	
-	public MersenneTwisterUniformRng(long seed){
+	/*
+	public MersenneTwisterUniformRng(int seed){
 		seedInitialization(seed);
 	}
 	
-	void seedInitialization(long seed){
+	
+	/**
+	 * Constructs and returns a random number generator seeded with the given date.
+	 *
+	 * @param d typically <tt>new java.util.Date()</tt>
+	 */
+	
+	/*
+	public MersenneTwisterUniformRng(Date d) {
+		this((int)d.getTime());
+	}
+	
+	
+	void seedInitialization(int seed){
 		
 		// iniitalizes mt with a seed 
-		long s = (seed !=0 ? SeedGenerator.getInstance());
+		int s = seed;
 		mt[0] = s & 0xffffffff;
-		for (mti = 1; mti<N; mti++) {
-			mt[mtj] = (1812433253 * (mt[mti-1]^(mt[mti-1] >> 30)) + mti);
+		for (int i = 1; i < N; i++) {
+			mt[i] = (1812433253 * (mt[i-1] ^ (mt[i-1] >> 30)) + i);
 			
 			// See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. 
 	        // In the previous versions, MSBs of the seed affect   
 	        // only MSBs of the array mt[].                        
 	        // 2002/01/09 modified by Makoto Matsumoto             
-	        mt[mti] &= 0xffffffff;
+	        mt[i] &= 0xffffffff;
 	        // for >32 bit machines 
 		}
+		mti = N;
 	}
 	
 	
-	void MersenneTwisterUniformRngByArray(ArrayList<Long> seeds){
+	protected void nextBlock(){
 		seedInitialization(19650218);
         int i=1;
         int j=0;
@@ -140,44 +170,22 @@ public class MersenneTwisterUniformRng {
 		
 	}
 	
-	
-	
-	public long nextInt32() {
+	public int nextInt32() {
 		
-		long y;
-        final long mag01[]={0x0, MATRIX_A};
-        // mag01[x] = x * MATRIX_A  for x=0,1 
+		// Each single bit including the sign bit will be random 
+	  	if (mti == N) {
+	  		nextBlock(); // generate N ints at one time
+	  	}
+	  	
+	  	int y = mt[mti++];
+		y ^= y >>> 11; // y ^= TEMPERING_SHIFT_U(y );
+		y ^= (y << 7) & TEMPERING_MASK_B; // y ^= TEMPERING_SHIFT_S(y) & TEMPERING_MASK_B;
+		y ^= (y << 15) & TEMPERING_MASK_C; // y ^= TEMPERING_SHIFT_T(y) & TEMPERING_MASK_C;	
+		// y &= 0xffffffff; //you may delete this line if word size = 32 
+		y ^= y >>> 18; // y ^= TEMPERING_SHIFT_L(y);
 
-        if (mti >= N) { // generate N words at one time 
-            int kk;
-
-            for (kk=0;kk<N-M;kk++) {
-                y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-                mt[kk] = mt[kk+M] ^ (y >> 1) ^ mag01[y & 0x1];
-            }
-            for (;kk<N-1;kk++) {
-                y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-                mt[kk] = mt[kk+(M-N)] ^ (y >> 1) ^ mag01[y & 0x1];
-            }
-            y = (mt[N-1]&UPPER_MASK)|(mt[0]&LOWER_MASK);
-            mt[N-1] = mt[M-1] ^ (y >> 1) ^ mag01[y & 0x1];
-
-            mti = 0;
-        }
-
-        y = mt[mti++];
-
-        // Tempering
-        y ^= (y >> 11);
-        y ^= (y << 7) & 0x9d2c5680;
-        y ^= (y << 15) & 0xefc60000;
-        y ^= (y >> 18);
-
-        return y;
-		
-	} 
-	
-	
+		return y;
+	}
 }
 
 */
