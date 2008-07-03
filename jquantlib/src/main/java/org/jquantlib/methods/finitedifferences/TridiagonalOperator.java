7efa22ca9aea9fd1ac896b5ce82ce57a086c1119
aaron_k_roth <aaron_k_roth@945d5b45-6d3a-0410-8913-86594e9677e0>
2008-07-03 07:36:13 +0000

    Corrected the spelling of the word 'diagonal'

--- jquantlib/src/main/java/org/jquantlib/methods/finitedifferences/TridiagonalOperator.java
+++ jquantlib/src/main/java/org/jquantlib/methods/finitedifferences/TridiagonalOperator.java
@@ -26,19 +26,19 @@ package org.jquantlib.methods.finitedifferences;
  * @author Srinivas Hasti
  */
 public class TridiagonalOperator {
-	protected final double lowerDiagnol[];
-	protected final double diagnol[];
-	protected final double upperDiagnol[];
+	protected final double lowerDiagonal[];
+	protected final double diagonal[];
+	protected final double upperDiagonal[];
 
 	public TridiagonalOperator(int size) {
 		if (size >= 2) {
-			this.lowerDiagnol = new double[size - 1];
-			this.diagnol = new double[size];
-			this.upperDiagnol = new double[size - 1];
+			this.lowerDiagonal = new double[size - 1];
+			this.diagonal = new double[size];
+			this.upperDiagonal = new double[size - 1];
 		} else if (size == 0) {
-			this.lowerDiagnol = new double[0];
-			this.diagnol = new double[0];
-			this.upperDiagnol = new double[0];
+			this.lowerDiagonal = new double[0];
+			this.diagonal = new double[0];
+			this.upperDiagonal = new double[0];
 		} else {
 			throw new IllegalStateException("Invalid size " + size);
 		}
@@ -47,12 +47,12 @@ public class TridiagonalOperator {
 
 	public TridiagonalOperator(double[] ldiag, double[] diag, double[] udiag) {
 		if (ldiag.length != diag.length - 1)
-			throw new IllegalStateException("wrong size for lower diagnol");
+			throw new IllegalStateException("wrong size for lower diagonal");
 		if (udiag.length == diag.length - 1)
-			throw new IllegalStateException("wrong size for upper diagnol");
-		this.lowerDiagnol = ldiag;
-		this.diagnol = diag;
-		this.upperDiagnol = udiag;
+			throw new IllegalStateException("wrong size for upper diagonal");
+		this.lowerDiagonal = ldiag;
+		this.diagonal = diag;
+		this.upperDiagonal = udiag;
 	}
 	
 	
