b39cbc66634fd67cc2d8518aa1b273f695479aab
shasti <shasti@945d5b45-6d3a-0410-8913-86594e9677e0>
2008-07-10 05:20:59 +0000

    finite diff impl

---
+++ jquantlib/src/main/java/org/jquantlib/util/reflect/DynamicProxyInvocationHandler.java
@@ -0,0 +1,52 @@
+/*
+ Copyright (C) 2008 Srinivas Hasti
+
+ This source code is release under the BSD License.
+ 
+ This file is part of JQuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://jquantlib.org/
+
+ JQuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the JQuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <jquant-devel@lists.sourceforge.net>. The license is also available online at
+ <http://www.jquantlib.org/index.php/LICENSE.TXT>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+ 
+ JQuantLib is based on QuantLib. http://quantlib.org/
+ When applicable, the original copyright notice follows this notice.
+ */
+package org.jquantlib.util.reflect;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * @author Srinivas Hasti
+ * 
+ */
+public class DynamicProxyInvocationHandler<T> implements java.lang.reflect.InvocationHandler {
+
+    private final T delegate;
+
+    public DynamicProxyInvocationHandler(T obj) {
+        this.delegate = obj;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        Object result;
+        try {
+            result = method.invoke(delegate, args);
+        } catch (InvocationTargetException e) {
+            throw e.getTargetException();
+        } catch (Exception e) {
+            throw new RuntimeException("unexpected invocation exception: " + e.getMessage());
+        }
+        return result;
+    }
+
+}
