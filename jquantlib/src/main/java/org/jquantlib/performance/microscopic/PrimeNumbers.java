d7ec4df1b8512761246cd792d6b6623d9e6af68b
u912boiler <u912boiler@945d5b45-6d3a-0410-8913-86594e9677e0>
2008-07-14 04:20:15 +0000

    Checking in a performance Framework.

---
+++ jquantlib/src/main/java/org/jquantlib/performance/microscopic/PrimeNumbers.java
@@ -0,0 +1,44 @@
+/*
+ Copyright (C) 2007 Richard Gomes
+ Copyright (C) 2008 Q. Boiler
+
+ This file is part of JQuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://jquantlib.org/
+
+ JQuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <jquant-devel@lists.sourceforge.net>. The license is also available online at
+ <http://www.jquantlib.org/index.php/LICENSE.TXT>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+ 
+ JQuantLib is based on QuantLib. http://quantlib.org/
+ When applicable, the original copyright notice follows this notice.
+ */
+package org.jquantlib.performance.microscopic;
+
+import org.jquantlib.performance.PerformanceResults;
+import org.jquantlib.performance.PerformanceTest;
+
+/**
+ *
+ * @author Q.Boiler
+ */
+public class PrimeNumbers implements PerformanceTest {
+
+	@Override
+	public PerformanceResults execute() {
+		org.jquantlib.math.PrimeNumbers pn;
+		PerformanceResults pr = new PerformanceResults();
+		pr.testName="Prime Numbers";
+		pr.units=PerformanceResults.RUNTIME_UNITS.NANOSECONDS;
+		pr.runtime=5;
+
+		return pr;
+	}
+
+
+}
