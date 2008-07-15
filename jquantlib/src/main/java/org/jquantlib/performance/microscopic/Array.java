98751cfa97b8780e916ef3b10210fdaaf8a53e0f
u912boiler <u912boiler@945d5b45-6d3a-0410-8913-86594e9677e0>
2008-07-15 05:42:23 +0000

    Added some tests.

--- jquantlib/src/main/java/org/jquantlib/performance/microscopic/Array.java
+++ jquantlib/src/main/java/org/jquantlib/performance/microscopic/Array.java
@@ -20,6 +20,10 @@
  */
 package org.jquantlib.performance.microscopic;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
 import org.jquantlib.performance.PerformanceResults;
 import org.jquantlib.performance.PerformanceTest;
 
@@ -29,15 +33,86 @@ import org.jquantlib.performance.PerformanceTest;
  */
 public class Array implements PerformanceTest {
 
+	Hashtable<Integer,List<org.jquantlib.math.Array>> ArraysHashTable
+		= new Hashtable<Integer,List<org.jquantlib.math.Array>>();
+	
 	@Override
 	public PerformanceResults execute() {
 		org.jquantlib.math.Array array;
 		PerformanceResults pr = new PerformanceResults();
-		pr.testName="Array Test";
-		pr.units=PerformanceResults.RUNTIME_UNITS.NANOSECONDS;
-		pr.runtime=5;
+		pr.testName="Array BuildArrays";
+		long startTime =System.currentTimeMillis();
 
+		ArraysHashTable.put(new Integer(10), PopulateArrays(10));
+		ArraysHashTable.put(new Integer(100), PopulateArrays(100));
+		ArraysHashTable.put(new Integer(1000), PopulateArrays(1000));
+		ArraysHashTable.put(new Integer(10000), PopulateArrays(10000));
+		ArraysHashTable.put(new Integer(100000), PopulateArrays(100000));
+		ArraysHashTable.put(new Integer(1000000), PopulateArrays(1000000));
+
+
+		pr.units=PerformanceResults.RUNTIME_UNITS.MILLISECONDS;
+		pr.runtime=System.currentTimeMillis()-startTime;
+
+		List<PerformanceResults> lists = executeDotProductCompositeTest();
+		pr.compositeResults = lists;
+
+		
 		return pr;
 	}
+	private List<PerformanceResults> executeDotProductCompositeTest(){
+		
+		List<PerformanceResults> dotProductResults = new ArrayList<PerformanceResults>();
+
+		for(int i =10;i<=1000000;i*=10){
+			PerformanceResults pr = executeDotProductTest(i);
+			dotProductResults.add(pr);
+
+		}
+		return dotProductResults;
+		
+	}
+	private List<org.jquantlib.math.Array> PopulateArrays(int size){
+
+		ArrayList<org.jquantlib.math.Array> list = new ArrayList<org.jquantlib.math.Array>();
+
+		for(int j =0;j<10;++j){
+		org.jquantlib.math.PrimeNumbers pn = new org.jquantlib.math.PrimeNumbers();
+
+		long prime = pn.get(7000);
+		org.jquantlib.math.randomnumbers.SFMTUniformRng rng
+			= new org.jquantlib.math.randomnumbers.SFMTUniformRng((int)prime);
+
+		org.jquantlib.math.Array a = new org.jquantlib.math.Array(size);
+		for(int i =0;i<size;++i){
+			a.set(i, (double)rng.next()/(double)prime);
+		}
+		list.add(a);
+		}
+
+		return list;
+	}
+	private PerformanceResults executeDotProductTest(int size){
+
+		PerformanceResults pr = new PerformanceResults();
+		pr.testName = "DotProduct Test "+size+": ";
+		long startTime = System.currentTimeMillis();
+		List<org.jquantlib.math.Array> localLists = ArraysHashTable.get(new Integer(size));
+		if(localLists == null){
+			//  Throw Exception.
+			System.out.println("Test Failed... for size: " +size);
+		}
+
+		Iterator iter =localLists.iterator();
+		org.jquantlib.math.Array arr = (org.jquantlib.math.Array)iter.next();
+		while(iter.hasNext()){
+			org.jquantlib.math.Array brr = (org.jquantlib.math.Array)iter.next();
+			arr.dotProduct(arr, brr);
+		}
+		pr.units=PerformanceResults.RUNTIME_UNITS.MILLISECONDS;
+		pr.runtime=System.currentTimeMillis()-startTime;
+		return pr;
+	}
+
 
 }
