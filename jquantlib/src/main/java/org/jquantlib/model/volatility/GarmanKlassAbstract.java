63c4c990e2ee81d4884fae8d0231988193f843e1
manianand74 <manianand74@945d5b45-6d3a-0410-8913-86594e9677e0>
2008-09-08 14:05:45 +0000

    corrected  formula in calculate method  ------- double s = Math.sqrt(Math.abs(calculatePoint(cur) / Math.sqrt(yearFraction))); ----- to  ------- double s = Math.sqrt(Math.abs(calculatePoint(cur)) / yearFraction); -----

--- jquantlib/src/main/java/org/jquantlib/model/volatility/GarmanKlassAbstract.java
+++ jquantlib/src/main/java/org/jquantlib/model/volatility/GarmanKlassAbstract.java
@@ -70,7 +70,7 @@ public abstract class GarmanKlassAbstract implements LocalVolatilityEstimator<In
 		IntervalPrice cur = null;
 		for (int i = 0; i < values.length; i++) {
 			cur = values[i];
-			double s = Math.sqrt(Math.abs(calculatePoint(cur) / Math.sqrt(yearFraction)));
+			double s = Math.sqrt(Math.abs(calculatePoint(cur)) / yearFraction);
 			retval.add(dates[i], s);
 		}
 		return retval;
