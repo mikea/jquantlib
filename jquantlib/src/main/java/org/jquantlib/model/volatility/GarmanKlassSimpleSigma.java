c2ebf5b3a0b6f3bf1954e9f6b8ed1edd20c42e08
manianand74 <manianand74@945d5b45-6d3a-0410-8913-86594e9677e0>
2008-09-08 14:06:32 +0000

    changed object Double to primitive double

--- jquantlib/src/main/java/org/jquantlib/model/volatility/GarmanKlassSimpleSigma.java
+++ jquantlib/src/main/java/org/jquantlib/model/volatility/GarmanKlassSimpleSigma.java
@@ -55,7 +55,7 @@ public class GarmanKlassSimpleSigma extends GarmanKlassAbstract {
 
 	@Override
 	protected double calculatePoint(final IntervalPrice p) {
-		Double c = Math.log(p.getClose() / p.getOpen());
+		double c = Math.log(p.getClose() / p.getOpen());
 		return c * c;
 	}
 
