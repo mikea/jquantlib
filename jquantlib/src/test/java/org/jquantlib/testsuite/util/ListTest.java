e27fa037a4d59fc844da3651e27af1d625c6c71b
u912boiler <u912boiler@945d5b45-6d3a-0410-8913-86594e9677e0>
2008-10-01 05:10:26 +0000

    Test Case Create a Decorator-Strategy Coupling for java.util.List to DoubleArrayList to Avoid invoking the AutoBoxing Functionality of Java.

---
+++ jquantlib/src/test/java/org/jquantlib/testsuite/util/ListTest.java
@@ -0,0 +1,67 @@
+/*
+ Copyright (C) 2008 Q Boiler
+
+ This source code is release under the BSD License.
+ 
+ This file is part of JQuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://jquantlib.org/
+
+ JQuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the JQuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <jquant-devel@lists.sourceforge.net>. The license is also available online at
+ <http://www.jquantlib.org/index.php/LICENSE.TXT>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+ 
+ JQuantLib is based on QuantLib. http://quantlib.org/
+ When applicable, the original copyright notice follows this notice.
+ */
+
+package org.jquantlib.testsuite.util;
+
+
+import it.unimi.dsi.fastutil.doubles.DoubleArrayList;
+
+import org.jquantlib.util.Date;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ListTest {
+
+    Date startDate = null;
+
+    @Before
+    public void init() {
+    }
+
+    @Test
+    public void testJavaUtilList() {
+		long startNano = System.nanoTime();
+		DoubleArrayList dal = new DoubleArrayList();
+		java.util.List list = dal;
+		for(double i = 0.0; i<1000 ; i=i+.01){
+			list.add(i);
+		}
+       
+		long totalTime = System.nanoTime() - startNano;
+		System.out.println("      java.util.list with  autoboxing : "+ totalTime +" nano seconds");
+    }
+
+
+    @Test
+    public void testJQuantList() { 
+		long startNano = System.nanoTime();
+		DoubleArrayList dal = new DoubleArrayList();
+		org.jquantlib.util.List list = new org.jquantlib.util.List(dal);
+		for(double i = 0.0; i<1000 ; i=i+.01){
+			list.add(i);
+		}
+		long totalTime = System.nanoTime() - startNano;
+
+		System.out.println("org.jqauntlib.util.List NO autoboxing : "+ totalTime +" nano seconds");
+    }
+
+}
