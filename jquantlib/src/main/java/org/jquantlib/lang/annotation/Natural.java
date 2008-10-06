b2ab480e733d567366374522238ae2e5d36221a6
fgomes <fgomes@945d5b45-6d3a-0410-8913-86594e9677e0>
2008-10-06 01:49:56 +0000

    Fixing Annotation types intended to guarantee semantic meaning at runtime. See JSR-308 :: http://groups.csail.mit.edu/pag/jsr308/

--- jquantlib/src/main/java/org/jquantlib/lang/annotation/Natural.java
+++ jquantlib/src/main/java/org/jquantlib/lang/annotation/Natural.java
@@ -10,6 +10,8 @@ import java.lang.annotation.Target;
  * guarantee that a field, local variable or parameter only assumes valid values.
  * 
  * @see <a href="http://en.wikipedia.org/wiki/Natural_numbers">Natural number</a>
+ * @see <a href="http://groups.csail.mit.edu/pag/jsr308/">JSR 308: Annotations on Java Types</a>
+ * @see <a href="http://www.jquantlib.org/index.php/Strong_Type_Checking">Strong Type Checking</a>  
  * 
  * @author Richard Gomes
  */
--- jquantlib/src/main/java/org/jquantlib/lang/annotation/PackagePrivate.java
+++ jquantlib/src/main/java/org/jquantlib/lang/annotation/PackagePrivate.java
@@ -6,8 +6,10 @@ import java.lang.annotation.Target;
 /**
  * This annotation is intended to mark a type as package private
  * 
+ * @see <a href="http://groups.csail.mit.edu/pag/jsr308/">JSR 308: Annotations on Java Types</a>
+ * @see <a href="http://www.jquantlib.org/index.php/Strong_Type_Checking">Strong Type Checking</a>
+ *   
  * @author Richard Gomes
- *
  */
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.TYPE})
 public @interface PackagePrivate {
