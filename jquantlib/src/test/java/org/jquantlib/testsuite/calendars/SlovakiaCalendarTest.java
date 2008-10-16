a19b85f9e273bdb1e19421387047138574e7b4cf
renjith4 <renjith4@945d5b45-6d3a-0410-8913-86594e9677e0>
2008-10-16 21:04:50 +0000

    Updated Slovakia calendar tests to complete the list 2004 - 2012

--- jquantlib/src/test/java/org/jquantlib/testsuite/calendars/SlovakiaCalendarTest.java
+++ jquantlib/src/test/java/org/jquantlib/testsuite/calendars/SlovakiaCalendarTest.java
@@ -25,12 +25,12 @@ package org.jquantlib.testsuite.calendars;
 import static org.jquantlib.util.Month.JANUARY;
 import static org.jquantlib.util.Month.MARCH;
 import static org.jquantlib.util.Month.APRIL;
+import static org.jquantlib.util.Month.MAY;
 import static org.jquantlib.util.Month.JULY;
 import static org.jquantlib.util.Month.AUGUST;
 import static org.jquantlib.util.Month.SEPTEMBER;
 import static org.jquantlib.util.Month.NOVEMBER;
 import static org.jquantlib.util.Month.DECEMBER;
-import static org.jquantlib.util.Month.APRIL;
 
 import java.util.List;
 import java.util.Vector;
@@ -39,14 +39,11 @@ import org.jquantlib.time.Calendar;
 import org.jquantlib.time.calendars.Slovakia;
 import org.jquantlib.util.Date;
 import org.jquantlib.util.DateFactory;
-import org.jquantlib.util.Month;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 /**
- * @author 
- *
+ * @author Heng Joon Tiang 
+ * @author Renjith Nair
  *
  */
 
@@ -61,12 +58,69 @@ public class SlovakiaCalendarTest {
                        
     	Calendar c = Slovakia.getCalendar(Slovakia.Market.BSSE);
     	
-        
-		// 2005 
+    	testSlovakiaYear2004(c);
         testSlovakiaYear2005(c);
+        testSlovakiaYear2006(c);
+        testSlovakiaYear2007(c);
+        testSlovakiaYear2008(c);
+        testSlovakiaYear2009(c);
+        testSlovakiaYear2010(c);
+        testSlovakiaYear2011(c);
+        testSlovakiaYear2012(c);
+        
+    }
+	
+	
+	//2004
+	void testSlovakiaYear2004(Calendar c) {
+      	int year = 2004;
+      	System.out.println("Testing Solvakia's holiday list for the year " + year + "...");
+    	List<Date> expectedHol = new Vector<Date>();
+		//New Year -- 1st Jan 
+		expectedHol.add(DateFactory.getFactory().getDate(1,JANUARY,year)); 
+    	
+		//Epiphany, January 6th
+		expectedHol.add(DateFactory.getFactory().getDate(6,JANUARY,year)); 
 		
-		// 2004 
-        testSlovakiaYear2004(c);
+		//Good Friday
+		expectedHol.add(DateFactory.getFactory().getDate(9,APRIL,year)); 
+		//Easter Monday
+		expectedHol.add(DateFactory.getFactory().getDate(12,APRIL,year)); 
+		
+		//May Day, May 1st -- weekend in yr 2004
+		//expectedHol.add(DateFactory.getFactory().getDate(1,MAY,year)); 
+		
+		//Liberation of the Republic, May 8th -- weekend in  yr 2004
+		//expectedHol.add(DateFactory.getFactory().getDate(8,MAY,year)); 
+		
+		//SS. Cyril and Methodius, July 5th
+		expectedHol.add(DateFactory.getFactory().getDate(5,JULY,year)); 
+		//Slovak National Uprising, August 29th -- weekend in yr 2004
+		//expectedHol.add(DateFactory.getFactory().getDate(29,AUGUST,year)); 
+		//Constitution of the Slovak Republic, September 1st
+    	expectedHol.add(DateFactory.getFactory().getDate(1,SEPTEMBER,year)); 
+		
+		//Our Lady of the Seven Sorrows, September 15th
+		expectedHol.add(DateFactory.getFactory().getDate(15,SEPTEMBER,year)); 
+		//All Saints Day, November 1st
+		expectedHol.add(DateFactory.getFactory().getDate(1,NOVEMBER,year)); 
+		//Freedom and Democracy of the Slovak Republic, November 17th
+		expectedHol.add(DateFactory.getFactory().getDate(17,NOVEMBER,year)); 
+		//Christmas Eve, December 24th 
+		expectedHol.add(DateFactory.getFactory().getDate(24,DECEMBER,year)); 
+		//Christmas, December 25th -- weekend in yr 2004
+		//expectedHol.add(DateFactory.getFactory().getDate(25,DECEMBER,year)); 
+		//St. Stephen, December 26th -- weekend in yr 2004
+		//expectedHol.add(DateFactory.getFactory().getDate(26,DECEMBER,year)); 
+		//unidentified stock exchange closing days 24-31 Dec, 24,25,26 Dec weekend in yr 2005
+		expectedHol.add(DateFactory.getFactory().getDate(27,DECEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(28,DECEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(29,DECEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(30,DECEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(31,DECEMBER,year)); 
+        // Call the Holiday Check
+        CalendarUtil cbt = new CalendarUtil();
+    	cbt.checkHolidayList(expectedHol, c, year);
     }
    
 	// 2005 
@@ -121,54 +175,173 @@ public class SlovakiaCalendarTest {
         CalendarUtil cbt = new CalendarUtil();
     	cbt.checkHolidayList(expectedHol, c, year);
     }
-	//2004
-	void testSlovakiaYear2004(Calendar c) {
-      	int year = 2004;
+	
+	
+	// 2006 - Year in the past 
+	void testSlovakiaYear2006(Calendar c) {
+      	
+		int year = 2006;
       	System.out.println("Testing Solvakia's holiday list for the year " + year + "...");
     	List<Date> expectedHol = new Vector<Date>();
-		//New Year -- 1st Jan 
-		expectedHol.add(DateFactory.getFactory().getDate(1,JANUARY,year)); 
-    	
-		//Epiphany, January 6th
+		
 		expectedHol.add(DateFactory.getFactory().getDate(6,JANUARY,year)); 
-		
-		//Good Friday
-		expectedHol.add(DateFactory.getFactory().getDate(9,APRIL,year)); 
-		//Easter Monday
-		expectedHol.add(DateFactory.getFactory().getDate(12,APRIL,year)); 
-		
-		//May Day, May 1st -- weekend in yr 2004
-		//expectedHol.add(DateFactory.getFactory().getDate(1,MAY,year)); 
-		
-		//Liberation of the Republic, May 8th -- weekend in  yr 2004
-		//expectedHol.add(DateFactory.getFactory().getDate(8,MAY,year)); 
-		
-		//SS. Cyril and Methodius, July 5th
+		expectedHol.add(DateFactory.getFactory().getDate(14,APRIL,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(17,APRIL,year));
+		expectedHol.add(DateFactory.getFactory().getDate(1,MAY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(8,MAY,year)); 
 		expectedHol.add(DateFactory.getFactory().getDate(5,JULY,year)); 
-		//Slovak National Uprising, August 29th -- weekend in yr 2004
-		//expectedHol.add(DateFactory.getFactory().getDate(29,AUGUST,year)); 
-		//Constitution of the Slovak Republic, September 1st
+		expectedHol.add(DateFactory.getFactory().getDate(29,AUGUST,year)); 
     	expectedHol.add(DateFactory.getFactory().getDate(1,SEPTEMBER,year)); 
-		
-		//Our Lady of the Seven Sorrows, September 15th
 		expectedHol.add(DateFactory.getFactory().getDate(15,SEPTEMBER,year)); 
-		//All Saints Day, November 1st
 		expectedHol.add(DateFactory.getFactory().getDate(1,NOVEMBER,year)); 
-		//Freedom and Democracy of the Slovak Republic, November 17th
 		expectedHol.add(DateFactory.getFactory().getDate(17,NOVEMBER,year)); 
-		//Christmas Eve, December 24th 
+		expectedHol.add(DateFactory.getFactory().getDate(25,DECEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(26,DECEMBER,year)); 
+        
+		// Call the Holiday Check
+        CalendarUtil cbt = new CalendarUtil();
+    	cbt.checkHolidayList(expectedHol, c, year);
+    }
+	
+	// 2007 - Year in the past 
+	void testSlovakiaYear2007(Calendar c) {
+      	
+		int year = 2007;
+      	System.out.println("Testing Solvakia's holiday list for the year " + year + "...");
+    	List<Date> expectedHol = new Vector<Date>();
+		
+    	expectedHol.add(DateFactory.getFactory().getDate(1,JANUARY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(6,APRIL,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(9,APRIL,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(1,MAY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(8,MAY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(5,JULY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(29,AUGUST,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(1,NOVEMBER,year)); 
 		expectedHol.add(DateFactory.getFactory().getDate(24,DECEMBER,year)); 
-		//Christmas, December 25th -- weekend in yr 2004
-		//expectedHol.add(DateFactory.getFactory().getDate(25,DECEMBER,year)); 
-		//St. Stephen, December 26th -- weekend in yr 2004
-		//expectedHol.add(DateFactory.getFactory().getDate(26,DECEMBER,year)); 
-		//unidentified stock exchange closing days 24-31 Dec, 24,25,26 Dec weekend in yr 2005
-		expectedHol.add(DateFactory.getFactory().getDate(27,DECEMBER,year)); 
-		expectedHol.add(DateFactory.getFactory().getDate(28,DECEMBER,year)); 
-		expectedHol.add(DateFactory.getFactory().getDate(29,DECEMBER,year)); 
-		expectedHol.add(DateFactory.getFactory().getDate(30,DECEMBER,year)); 
-		expectedHol.add(DateFactory.getFactory().getDate(31,DECEMBER,year)); 
-        // Call the Holiday Check
+		expectedHol.add(DateFactory.getFactory().getDate(25,DECEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(26,DECEMBER,year));
+
+		// Call the Holiday Check
+        CalendarUtil cbt = new CalendarUtil();
+    	cbt.checkHolidayList(expectedHol, c, year);
+    }
+	
+	// 2008 - Current Year
+	void testSlovakiaYear2008(Calendar c) {
+      	
+		int year = 2008;
+      	System.out.println("Testing Solvakia's holiday list for the year " + year + "...");
+    	List<Date> expectedHol = new Vector<Date>();
+		
+    	expectedHol.add(DateFactory.getFactory().getDate(1,JANUARY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(21,MARCH,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(24,MARCH,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(1,MAY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(8,MAY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(29,AUGUST,year)); 
+    	expectedHol.add(DateFactory.getFactory().getDate(1,SEPTEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(15,SEPTEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(17,NOVEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(24,DECEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(25,DECEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(26,DECEMBER,year)); 
+
+		// Call the Holiday Check
+        CalendarUtil cbt = new CalendarUtil();
+    	cbt.checkHolidayList(expectedHol, c, year);
+    }
+	
+	// 2009 - Future Year
+	void testSlovakiaYear2009(Calendar c) {
+      	
+		int year = 2009;
+      	System.out.println("Testing Solvakia's holiday list for the year " + year + "...");
+    	List<Date> expectedHol = new Vector<Date>();
+		
+    	expectedHol.add(DateFactory.getFactory().getDate(1,JANUARY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(6,JANUARY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(10,APRIL,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(13,APRIL,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(1,MAY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(8,MAY,year)); 
+    	expectedHol.add(DateFactory.getFactory().getDate(1,SEPTEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(15,SEPTEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(17,NOVEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(24,DECEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(25,DECEMBER,year)); 
+		
+		// Call the Holiday Check
+        CalendarUtil cbt = new CalendarUtil();
+    	cbt.checkHolidayList(expectedHol, c, year);
+    }
+	
+	// 2010 - Future Year
+	void testSlovakiaYear2010(Calendar c) {
+      	
+		int year = 2010;
+      	System.out.println("Testing Solvakia's holiday list for the year " + year + "...");
+    	List<Date> expectedHol = new Vector<Date>();
+		
+    	expectedHol.add(DateFactory.getFactory().getDate(1,JANUARY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(6,JANUARY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(2,APRIL,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(5,APRIL,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(5,JULY,year)); 
+    	expectedHol.add(DateFactory.getFactory().getDate(1,SEPTEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(15,SEPTEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(1,NOVEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(17,NOVEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(24,DECEMBER,year)); 
+        
+		// Call the Holiday Check
+        CalendarUtil cbt = new CalendarUtil();
+    	cbt.checkHolidayList(expectedHol, c, year);
+    }
+	
+	// 2011 - Future Year
+	void testSlovakiaYear2011(Calendar c) {
+      	
+		int year = 2011;
+      	System.out.println("Testing Solvakia's holiday list for the year " + year + "...");
+    	List<Date> expectedHol = new Vector<Date>();
+		
+		expectedHol.add(DateFactory.getFactory().getDate(6,JANUARY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(22,APRIL,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(25,APRIL,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(5,JULY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(29,AUGUST,year)); 
+    	expectedHol.add(DateFactory.getFactory().getDate(1,SEPTEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(15,SEPTEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(1,NOVEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(17,NOVEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(26,DECEMBER,year)); 
+        
+		// Call the Holiday Check
+        CalendarUtil cbt = new CalendarUtil();
+    	cbt.checkHolidayList(expectedHol, c, year);
+    }
+	
+	// 2012 - Future Year
+	void testSlovakiaYear2012(Calendar c) {
+      	
+		int year = 2012;
+      	System.out.println("Testing Solvakia's holiday list for the year " + year + "...");
+    	List<Date> expectedHol = new Vector<Date>();
+		
+		expectedHol.add(DateFactory.getFactory().getDate(6,JANUARY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(6,APRIL,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(9,APRIL,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(1,MAY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(8,MAY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(5,JULY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(29,AUGUST,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(1,NOVEMBER,year));
+		expectedHol.add(DateFactory.getFactory().getDate(24,DECEMBER,year));
+		expectedHol.add(DateFactory.getFactory().getDate(25,DECEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(26,DECEMBER,year)); 
+        
+		// Call the Holiday Check
         CalendarUtil cbt = new CalendarUtil();
     	cbt.checkHolidayList(expectedHol, c, year);
     }
