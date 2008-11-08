e32c6a1573716c66da39ab042950e8ea26635121
renjith4 <renjith4@945d5b45-6d3a-0410-8913-86594e9677e0>
2008-11-08 20:58:34 +0000

    Completed SaudiArabia Calendar Test Stack 2004 - 2012

--- jquantlib/src/test/java/org/jquantlib/testsuite/calendars/SaudiArabiaCalendarTest.java
+++ jquantlib/src/test/java/org/jquantlib/testsuite/calendars/SaudiArabiaCalendarTest.java
@@ -25,6 +25,7 @@ package org.jquantlib.testsuite.calendars;
 import static org.jquantlib.util.Month.FEBRUARY;
 import static org.jquantlib.util.Month.JANUARY;
 import static org.jquantlib.util.Month.NOVEMBER;
+import static org.jquantlib.util.Month.SEPTEMBER;
 
 import java.util.List;
 import java.util.Vector;
@@ -38,8 +39,8 @@ import org.jquantlib.util.DateFactory;
 import org.junit.Test;
 
 /**
- * @author
- *
+ * @author Joon Tiang
+ * @author Renjith Nair
  *
  */
 
@@ -57,32 +58,27 @@ public class SaudiArabiaCalendarTest {
                        
     	Calendar c = SaudiArabia.getCalendar(SaudiArabia.Market.TADAWUL);
     	
-        // 2005 
-        testSaudiArabiaYear2005(c);
-		//2004
+        //2004
 		testSaudiArabiaYear2004(c);
+		// 2005 
+        testSaudiArabiaYear2005(c);
+        // 2006 
+        testSaudiArabiaYear2006(c);
+        // 2007 
+        testSaudiArabiaYear2007(c);
+        // 2008 
+        testSaudiArabiaYear2008(c);
+        // 2009 
+        testSaudiArabiaYear2009(c);
+        // 2010 
+        testSaudiArabiaYear2010(c);
+        // 2011 
+        testSaudiArabiaYear2011(c);
+        // 2012 
+        testSaudiArabiaYear2012(c);
 		
     }
-    // 2005
-	void testSaudiArabiaYear2005(Calendar c) {
-      	int year = 2005;
-      	logger.info("Testing SaudiArabia's holiday list for the year " + year + "...");
-    	List<Date> expectedHol = new Vector<Date>();
-		// Eid Al-Adha -- 21 is on Fri, weekend in yr 2005
-		expectedHol.add(DateFactory.getFactory().getDate(22,JANUARY,year)); 
-		expectedHol.add(DateFactory.getFactory().getDate(23,JANUARY,year)); 
-		expectedHol.add(DateFactory.getFactory().getDate(24,JANUARY,year)); 
-		expectedHol.add(DateFactory.getFactory().getDate(25,JANUARY,year)); 
-    	//National Day -- weekend in 2005
-    	//expectedHol.add(DateFactory.getFactory().getDate(23,SEPTEMBER,year)); 
-		// Eid Al-Fitr -- 17,18 falls on thur,fri , weekend in yr 2005
-        expectedHol.add(DateFactory.getFactory().getDate(14,NOVEMBER,year)); 
-		expectedHol.add(DateFactory.getFactory().getDate(15,NOVEMBER,year)); 
-		expectedHol.add(DateFactory.getFactory().getDate(16,NOVEMBER,year)); 
-        // Call the Holiday Check
-        CalendarUtil cbt = new CalendarUtil();
-    	cbt.checkHolidayList(expectedHol, c, year);
-    }
+    
 	//2004
 	void testSaudiArabiaYear2004(Calendar c) {
       	int year = 2004;
@@ -104,9 +100,117 @@ public class SaudiArabiaCalendarTest {
     	cbt.checkHolidayList(expectedHol, c, year);
     }
 	
-    
+	// 2005
+	void testSaudiArabiaYear2005(Calendar c) {
+      	int year = 2005;
+      	logger.info("Testing SaudiArabia's holiday list for the year " + year + "...");
+    	List<Date> expectedHol = new Vector<Date>();
+		// Eid Al-Adha -- 21 is on Fri, weekend in yr 2005
+		expectedHol.add(DateFactory.getFactory().getDate(22,JANUARY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(23,JANUARY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(24,JANUARY,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(25,JANUARY,year)); 
+    	//National Day -- weekend in 2005
+    	//expectedHol.add(DateFactory.getFactory().getDate(23,SEPTEMBER,year)); 
+		// Eid Al-Fitr -- 17,18 falls on thur,fri , weekend in yr 2005
+        expectedHol.add(DateFactory.getFactory().getDate(14,NOVEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(15,NOVEMBER,year)); 
+		expectedHol.add(DateFactory.getFactory().getDate(16,NOVEMBER,year)); 
+        // Call the Holiday Check
+        CalendarUtil cbt = new CalendarUtil();
+    	cbt.checkHolidayList(expectedHol, c, year);
+    }
+	// 2006
+	void testSaudiArabiaYear2006(Calendar c) {
+      	int year = 2006;
+      	logger.info("Testing SaudiArabia's holiday list for the year " + year + "...");
+    	
+      	List<Date> expectedHol = new Vector<Date>();
+    	//National Day 
+    	expectedHol.add(DateFactory.getFactory().getDate(23,SEPTEMBER,year)); 
+        
+      	// Call the Holiday Check        
+      	CalendarUtil cbt = new CalendarUtil();
+    	cbt.checkHolidayList(expectedHol, c, year);
+    }
 	
+	// 2007
+	void testSaudiArabiaYear2007(Calendar c) {
+      	int year = 2007;
+      	logger.info("Testing SaudiArabia's holiday list for the year " + year + "...");
+      	List<Date> expectedHol = new Vector<Date>();
+    	//National Day 
+    	expectedHol.add(DateFactory.getFactory().getDate(23,SEPTEMBER,year)); 
+        
+      	// Call the Holiday Check
+        CalendarUtil cbt = new CalendarUtil();
+    	cbt.checkHolidayList(expectedHol, c, year);
+    }
 	
+	// 2008
+	void testSaudiArabiaYear2008(Calendar c) {
+      	int year = 2008;
+      	logger.info("Testing SaudiArabia's holiday list for the year " + year + "...");
+      	List<Date> expectedHol = new Vector<Date>();
+    	//National Day 
+    	expectedHol.add(DateFactory.getFactory().getDate(23,SEPTEMBER,year)); 
+        
+      	// Call the Holiday Check
+        CalendarUtil cbt = new CalendarUtil();
+    	cbt.checkHolidayList(expectedHol, c, year);
+    }
+	
+	// 2009
+	void testSaudiArabiaYear2009(Calendar c) {
+      	int year = 2009;
+      	logger.info("Testing SaudiArabia's holiday list for the year " + year + "...");
+      	List<Date> expectedHol = new Vector<Date>();
+    	//National Day 
+    	expectedHol.add(DateFactory.getFactory().getDate(23,SEPTEMBER,year)); 
+        
+      	// Call the Holiday Check
+        CalendarUtil cbt = new CalendarUtil();
+    	cbt.checkHolidayList(expectedHol, c, year);
+    }
+	
+	// 2010
+	void testSaudiArabiaYear2010(Calendar c) {
+      	int year = 2010;
+      	logger.info("Testing SaudiArabia's holiday list for the year " + year + "...");
+      	List<Date> expectedHol = new Vector<Date>();
+    	//National Day - On a Thursday and hence a weekend
+    	//expectedHol.add(DateFactory.getFactory().getDate(23,SEPTEMBER,year)); 
+        
+      	// Call the Holiday Check
+        CalendarUtil cbt = new CalendarUtil();
+    	cbt.checkHolidayList(expectedHol, c, year);
+    }
+	
+	// 2011
+	void testSaudiArabiaYear2011(Calendar c) {
+      	int year = 2011;
+      	logger.info("Testing SaudiArabia's holiday list for the year " + year + "...");
+      	List<Date> expectedHol = new Vector<Date>();
+    	//National Day On a Friday and hence a weekend
+    	//expectedHol.add(DateFactory.getFactory().getDate(23,SEPTEMBER,year)); 
+        
+      	// Call the Holiday Check
+        CalendarUtil cbt = new CalendarUtil();
+    	cbt.checkHolidayList(expectedHol, c, year);
+    }
+	
+	// 2012
+	void testSaudiArabiaYear2012(Calendar c) {
+      	int year = 2012;
+      	logger.info("Testing SaudiArabia's holiday list for the year " + year + "...");
+      	List<Date> expectedHol = new Vector<Date>();
+    	//National Day 
+    	expectedHol.add(DateFactory.getFactory().getDate(23,SEPTEMBER,year)); 
+        
+      	// Call the Holiday Check
+        CalendarUtil cbt = new CalendarUtil();
+    	cbt.checkHolidayList(expectedHol, c, year);
+    }
 	
 }
 
