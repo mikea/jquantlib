01ace06ddebb2cd8af22bba3622ac36f72226513
shasti <shasti@945d5b45-6d3a-0410-8913-86594e9677e0>
2009-02-09 04:22:22 +0000

    American options pricing

---
+++ jquantlib/src/main/java/org/jquantlib/pricingengines/vanilla/finitedifferences/FDShoutEngine.java
@@ -0,0 +1,45 @@
+/*
+ Copyright (C) 2007 Srinivas Hasti
+
+ This file is part of JQuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://jquantlib.org/
+
+ JQuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <jquant-devel@lists.sourceforge.net>. The license is also available online at
+ <http://www.jquantlib.org/index.php/LICENSE.TXT>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+ 
+ JQuantLib is based on QuantLib. http://quantlib.org/
+ When applicable, the original copyright notice follows this notice.
+ */
+package org.jquantlib.pricingengines.vanilla.finitedifferences;
+
+import org.jquantlib.processes.BlackScholesMertonProcess;
+import org.jquantlib.processes.GeneralizedBlackScholesProcess;
+
+/**
+ * @author Srinivas Hasti
+ * 
+ */
+public class FDShoutEngine extends FDEngineAdapter<FDShoutCondition> {
+
+	public FDShoutEngine(GeneralizedBlackScholesProcess process, int timeSteps,
+			int gridPoints, boolean timeDependent) {
+		super(process, timeSteps, gridPoints, timeDependent);
+	}
+
+	public FDShoutEngine(GeneralizedBlackScholesProcess process, int timeSteps,
+			int gridPoints) {
+		this(process, timeSteps, gridPoints, false);
+	}
+
+	public FDShoutEngine(BlackScholesMertonProcess process) {
+		this(process, 100, 100, false);
+	}
+
+}
