65ea829806bc22ee49d618263cccf9ea8300d276
shasti <shasti@945d5b45-6d3a-0410-8913-86594e9677e0>
2009-02-20 12:42:05 +0000

    binomial engines

---
+++ jquantlib/src/main/java/org/jquantlib/pricingengines/vanilla/JarrowRuddBinomialVanillaEngine.java
@@ -0,0 +1,37 @@
+/*
+ Copyright (C) 2007 Srinivas Hasti
+
+ This file is part of JQuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://jquantlib.org/
+
+ JQuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <jquant-devel@lists.sourceforge.net>. The license is also available online at
+ <http://www.jquantlib.org/index.php/LICENSE.TXT>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+ 
+ JQuantLib is based on QuantLib. http://quantlib.org/
+ When applicable, the original copyright notice follows this notice.
+ */
+package org.jquantlib.pricingengines.vanilla;
+
+import org.jquantlib.methods.lattices.JarrowRudd;
+import org.jquantlib.pricingengines.vanilla.AbstractBinomialVanillaEngine;
+import org.jquantlib.processes.GeneralizedBlackScholesProcess;
+
+/**
+ * @author Srinivas Hasti
+ * 
+ */
+public class JarrowRuddBinomialVanillaEngine extends
+		AbstractBinomialVanillaEngine<JarrowRudd> {
+
+	public JarrowRuddBinomialVanillaEngine(
+			GeneralizedBlackScholesProcess process, int timeSteps) {
+		super(process, timeSteps);
+	}
+}
