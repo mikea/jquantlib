b68683b54c4a5ca84dea99217d7e43cf2bea41fc
hofi85 <hofi85@945d5b45-6d3a-0410-8913-86594e9677e0>
2009-03-09 22:46:26 +0000

    translated cpp - needs review

--- jquantlib/src/main/java/org/jquantlib/pricingengines/vanilla/finitedifferences/FDMultiPeriodEngine.java
+++ jquantlib/src/main/java/org/jquantlib/pricingengines/vanilla/finitedifferences/FDMultiPeriodEngine.java
@@ -1,12 +1,42 @@
+/*
+ Copyright (C) 2009 Ueli Hofstetter
+
+ This file is part of JQuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://jquantlib.org/
+
+ JQuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <jquant-devel@lists.sourceforge.net>. The license is also available online at
+ <http://www.jquantlib.org/index.php/LICENSE.TXT>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+ 
+ JQuantLib is based on QuantLib. http://quantlib.org/
+ When applicable, the original copyright notice follows this notice.
+ */
 package org.jquantlib.pricingengines.vanilla.finitedifferences;
 
+
 import org.jquantlib.cashflow.Event;
+import org.jquantlib.math.Array;
 import org.jquantlib.math.SampledCurve;
+import org.jquantlib.methods.finitedifferences.NullCondition;
 import org.jquantlib.methods.finitedifferences.StandardFiniteDifferenceModel;
 import org.jquantlib.methods.finitedifferences.StepCondition;
 import org.jquantlib.pricingengines.arguments.Arguments;
 import org.jquantlib.pricingengines.arguments.OneAssetOptionArguments;
+import org.jquantlib.pricingengines.results.OptionResults;
+import org.jquantlib.pricingengines.results.Results;
 import org.jquantlib.processes.GeneralizedBlackScholesProcess;
+
+/**
+ * 
+ * This class needs review!!!!!!!!!!!!!!!!
+ *
+ */
 
 public class FDMultiPeriodEngine extends FDVanillaEngine {
     
@@ -54,123 +84,52 @@ public class FDMultiPeriodEngine extends FDVanillaEngine {
     double getDividendTime(int i){
         return stoppingTimes_[i];
     }
-
-}
-
-
-/*#ifndef quantlib_fd_multi_period_engine_hpp
-#define quantlib_fd_multi_period_engine_hpp
-
-#include <ql/pricingengines/vanilla/fdvanillaengine.hpp>
-#include <ql/methods/finitedifferences/fdtypedefs.hpp>
-#include <ql/instruments/oneassetoption.hpp>
-#include <ql/event.hpp>
-
-namespace QuantLib {
-
-    class FDMultiPeriodEngine : public FDVanillaEngine {
-      protected:
-        FDMultiPeriodEngine(Size gridPoints=100, Size timeSteps=100,
-                            bool timeDependent = false);
-        mutable std::vector<boost::shared_ptr<Event> > events_;
-        mutable std::vector<Time> stoppingTimes_;
-        Size timeStepPerPeriod_;
-        mutable SampledCurve prices_;
-        virtual void setupArguments(
-               const PricingEngine::arguments* args,
-               const std::vector<boost::shared_ptr<Event> >& schedule) const {
-            FDVanillaEngine::setupArguments(args);
-            events_ = schedule;
-            stoppingTimes_.clear();
-            Size n = schedule.size();
-            stoppingTimes_.reserve(n);
-            for (Size i=0; i<n; ++i)
-                stoppingTimes_.push_back(process_->time(events_[i]->date()));
-        };
-        virtual void setupArguments(const PricingEngine::arguments* a) const {
-            FDVanillaEngine::setupArguments(a);
-            const OneAssetOption::arguments *args =
-                dynamic_cast<const OneAssetOption::arguments*>(a);
-            QL_REQUIRE(args, "incorrect argument type");
-            events_.clear();
-            stoppingTimes_ = args->stoppingTimes;
-        };
-
-        virtual void calculate(PricingEngine::results*) const;
-        mutable boost::shared_ptr<StandardStepCondition > stepCondition_;
-        mutable boost::shared_ptr<StandardFiniteDifferenceModel> model_;
-        virtual void executeIntermediateStep(Size step) const = 0;
-        virtual void initializeStepCondition() const;
-        virtual void initializeModel() const;
-        Time getDividendTime(Size i) const {
-            return stoppingTimes_[i];
-        }
-    };
-
-}
-
-
-#endif
-
-
-#include <ql/pricingengines/vanilla/fdmultiperiodengine.hpp>
-
-namespace QuantLib {
-    FDMultiPeriodEngine::
-    FDMultiPeriodEngine(Size gridPoints, Size timeSteps,
-                        bool timeDependent)
-    : FDVanillaEngine(gridPoints, timeSteps, timeDependent),
-      timeStepPerPeriod_(timeSteps) {}
-
-    void FDMultiPeriodEngine::calculate(PricingEngine::results* r) const {
-        OneAssetOption::results *results =
-            dynamic_cast<OneAssetOption::results *>(r);
-        QL_REQUIRE(results, "incorrect argument type");
-        Time beginDate, endDate;
-        Size dateNumber = stoppingTimes_.size();
-        bool lastDateIsResTime = false;
-        Integer firstIndex = -1;
-        Integer lastIndex = dateNumber - 1;
-        bool firstDateIsZero = false;
-        Time firstNonZeroDate = getResidualTime();
-
-        Real dateTolerance = 1e-6;
-
-        if (dateNumber > 0){
-            QL_REQUIRE(getDividendTime(0) >= 0,
-                       "first date (" << getDividendTime(0)
-                       << ") cannot be negative");
-            if(getDividendTime(0) < getResidualTime() * dateTolerance ){
+    
+    public void calculate(Results r){
+        double beginDate, endDate;
+        int dateNumber = stoppingTimes_.length;
+        boolean lastDateIsResTime = false;
+        int firstIndex = -1;
+        int lastIndex = dateNumber - 1;
+        boolean firstDateIsZero = false;
+        double firstNonZeroDate = getResidualTime();
+        
+        double dateTolerance = 1e-6;
+        
+        if(dateNumber>0){
+            if(getDividendTime(0)<=0){
+                throw new IllegalArgumentException("first date (" + getDividendTime(0) + ") cannot be negative");
+            }
+            if(getDividendTime(0)<getResidualTime() *dateTolerance){
                 firstDateIsZero = true;
                 firstIndex = 0;
-                if(dateNumber >= 2)
+                if(dateNumber >= 2){
                     firstNonZeroDate = getDividendTime(1);
+                }
             }
+            if(Math.abs(getDividendTime(lastIndex) - getResidualTime())<dateTolerance){
+                    lastDateIsResTime = true;
+                    lastIndex = dateNumber - 2;
+                }
 
-            if (std::fabs(getDividendTime(lastIndex) - getResidualTime())
-                < dateTolerance) {
-                lastDateIsResTime = true;
-                lastIndex = Integer(dateNumber) - 2;
-            }
+                if (!firstDateIsZero){
+                    firstNonZeroDate = getDividendTime(0);
+                }
 
-            if (!firstDateIsZero)
-                firstNonZeroDate = getDividendTime(0);
-
-            if (dateNumber >= 2) {
-                for (Size j = 1; j < dateNumber; j++)
-                    QL_REQUIRE(getDividendTime(j-1) < getDividendTime(j),
-                               "dates must be in increasing order: "
-                               << getDividendTime(j-1)
-                               << " is not strictly smaller than "
-                               << getDividendTime(j));
-            }
+                if (dateNumber >= 2) {
+                    for (int j = 1; j < dateNumber; j++)
+                        if(getDividendTime(j-1) > getDividendTime(j)){
+                            throw new IllegalArgumentException("dates must be in increasing order: "
+                                   + getDividendTime(j-1) +" is not strictly smaller than " + getDividendTime(j));
+                        }       
+                }
         }
-
-        Time dt = getResidualTime()/(timeStepPerPeriod_*(dateNumber+1));
+        double dt = getResidualTime()/(timeStepPerPeriod_*(dateNumber+1));
 
         // Ensure that dt is always smaller than the first non-zero date
-        if (firstNonZeroDate <= dt)
+        if (firstNonZeroDate <= dt){
             dt = firstNonZeroDate/2.0;
+        }
 
         setGridLimits();
         initializeInitialCondition();
@@ -179,54 +138,55 @@ namespace QuantLib {
         initializeModel();
         initializeStepCondition();
 
-        prices_ = intrinsicValues_;
-        if(lastDateIsResTime)
-            executeIntermediateStep(dateNumber - 1);
+        prices_ = intrinsicValues;
+        if(lastDateIsResTime){
+            //FIXME:not implementd yet
+            //executeIntermediateStep(dateNumber - 1);
+        }
 
         Integer j = lastIndex;
         do{
-            if (j == Integer(dateNumber) - 1)
+            if (j == (dateNumber - 1)){
                 beginDate = getResidualTime();
-            else
+            }
+            else{
                 beginDate = getDividendTime(j+1);
+            }
 
-            if (j >= 0)
+            if (j >= 0){
                 endDate = getDividendTime(j);
-            else
+            }
+            else{
                 endDate = dt;
+            }
 
-            model_->rollback(prices_.values(),
-                             beginDate, endDate,
-                             timeStepPerPeriod_, *stepCondition_);
-            if (j >= 0)
-                executeIntermediateStep(j);
+            model_.rollback(prices_.values(),beginDate, endDate,timeStepPerPeriod_, stepCondition_);
+            
+            if (j >= 0){
+                //FIXME: not implementd yet
+                //executeIntermediateStep(j);
+            }
         } while (--j >= firstIndex);
 
-        model_->rollback(prices_.values(), dt, 0, 1, *stepCondition_);
+        model_.rollback(prices_.values(),dt, 0, 1, stepCondition_);
+        
 
-        if(firstDateIsZero)
-            executeIntermediateStep(0);
+        if(firstDateIsZero){
+            //FIXME: not implementd yet
+            //executeIntermediateStep(0);
+        }
 
-        results->value = prices_.valueAtCenter();
-        results->delta = prices_.firstDerivativeAtCenter();
-        results->gamma = prices_.secondDerivativeAtCenter();
-        results->additionalResults["priceCurve"] = prices_;
+        ((OptionResults)r).value = prices_.valueAtCenter();
+        ((OptionResults)r).delta = prices_.firstDerivativeAtCenter();
+        ((OptionResults)r).gamma = prices_.secondDerivativeAtCenter();
+        ((OptionResults)r).addAdditionalResult("priceCurve", prices_);
+    }
+    
+    void initializeStepCondition() {
+        stepCondition_ = new NullCondition<Array>();
     }
 
-    void FDMultiPeriodEngine::initializeStepCondition() const{
-        stepCondition_ = boost::shared_ptr<StandardStepCondition>(
-                                                  new NullCondition<Array>());
+    void initializeModel() {
+        model_ = new StandardFiniteDifferenceModel(finiteDifferenceOperator, bcS);
     }
-
-    void FDMultiPeriodEngine::initializeModel() const{
-        model_ = boost::shared_ptr<StandardFiniteDifferenceModel>(
-           new StandardFiniteDifferenceModel(finiteDifferenceOperator_,BCs_));
-    }
-
 }
-
-
-
-
-
-*/
