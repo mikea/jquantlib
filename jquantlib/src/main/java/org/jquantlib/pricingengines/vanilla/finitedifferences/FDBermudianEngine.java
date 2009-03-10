b39c08ce646c2d09f4921a3c505ca6a9d7d727d8
hofi85 <hofi85@945d5b45-6d3a-0410-8913-86594e9677e0>
2009-03-10 12:13:13 +0000

    started translating header file

--- jquantlib/src/main/java/org/jquantlib/pricingengines/vanilla/finitedifferences/FDBermudianEngine.java
+++ jquantlib/src/main/java/org/jquantlib/pricingengines/vanilla/finitedifferences/FDBermudianEngine.java
@@ -1,11 +1,63 @@
+/*
+ Copyright (C) 2009 Ueli Hofstetter
+
+ This file is part of JQuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://jquantlib.org/
+
+ JQuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <jquant-devel@lists.sourceforge.net>. The license is also available online at
+ <http://www.jquantlib.org/index.php/LICENSE.TXT>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+ 
+ JQuantLib is based on QuantLib. http://quantlib.org/
+ When applicable, the original copyright notice follows this notice.
+ */
+
 package org.jquantlib.pricingengines.vanilla.finitedifferences;
 
-import org.jquantlib.pricingengines.VanillaOptionEngine;
+import org.jquantlib.math.Array;
+import org.jquantlib.methods.finitedifferences.NullCondition;
+import org.jquantlib.pricingengines.results.Results;
+import org.jquantlib.processes.GeneralizedBlackScholesProcess;
 
 //work in progress
 
+public class FDBermudianEngine extends FDMultiPeriodEngine{
 
-public class FDBermudianEngine {
+	double extraTermInBermuda;
+	
+	public FDBermudianEngine(GeneralizedBlackScholesProcess process,
+			int timeSteps, int gridPoints, boolean timeDependent) {
+		/* default values. where to set them?
+		timeSteps = 100;
+		gridPoints = 100;
+		timeDependent = false;
+		*/
+		super(process, timeSteps, gridPoints, timeDependent);
+	}
+
+	@Override
+	public void calculate(Results r){
+		//FIXME: derive arguments from fdmultiperiodengine
+		//setupArguments()
+		//setupA
+	}
+	
+	void initializeStepCondition(){
+		stepCondition_ = new NullCondition<Array>();
+	}
+	
+	void executeIntermediateStep(int step){
+		int size = intrinsicValues.size();
+		for(int j = 0; j<size; j++){
+			prices_.values().set(j, Math.max(prices_.value(j), intrinsicValues.value(j)));
+			}
+	}
 
 }
 
