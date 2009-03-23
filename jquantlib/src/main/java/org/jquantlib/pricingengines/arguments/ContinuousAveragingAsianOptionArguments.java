d7d02b2c2043887a66f2c32860eac541c60bec56
hofi85 <hofi85@945d5b45-6d3a-0410-8913-86594e9677e0>
2009-03-23 00:16:33 +0000

    fixed initialization

--- jquantlib/src/main/java/org/jquantlib/pricingengines/arguments/ContinuousAveragingAsianOptionArguments.java
+++ jquantlib/src/main/java/org/jquantlib/pricingengines/arguments/ContinuousAveragingAsianOptionArguments.java
@@ -1,21 +1,59 @@
+/*
+ Copyright (C) 2008 Richard Gomes, Ueli Hofstetter
+
+ This source code is release under the BSD License.
+ 
+ This file is part of JQuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://jquantlib.org/
+
+ JQuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the JQuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <jquant-devel@lists.sourceforge.net>. The license is also available online at
+ <http://www.jquantlib.org/index.php/LICENSE.TXT>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+ 
+ JQuantLib is based on QuantLib. http://quantlib.org/
+ When applicable, the original copyright notice follows this notice.
+ */
+
+/*
+ Copyright (C) 2000, 2001, 2002, 2003 RiskMap srl
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
 package org.jquantlib.pricingengines.arguments;
 
 import org.jquantlib.instruments.AverageType;
 
 public class ContinuousAveragingAsianOptionArguments extends OneAssetStrikedOptionArguments {
 
+    public static final String unspecified_average_type = "unspecified average type";
+    
 	public ContinuousAveragingAsianOptionArguments() {
-		//averageType(Average::Type(-1)) {}
-
 	}
     
 	@Override
 	public void validate() /*@ReadOnly*/{
         super.validate();
-        //FIXME review interpretation of enum=-1...
-        //QL_REQUIRE(Integer(averageType) != -1, "unspecified average type");
+        //check whether null instead of using -1 enum typ
         if(averageType==null){
-        	throw new IllegalArgumentException("unspecified average type");
+        	throw new IllegalArgumentException(unspecified_average_type);
         }
 
     }
