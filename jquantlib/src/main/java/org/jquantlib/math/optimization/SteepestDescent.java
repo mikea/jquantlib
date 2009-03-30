3a51db16ab49c46b298dc86e930efc1bb896b71c
hofi85 <hofi85@945d5b45-6d3a-0410-8913-86594e9677e0>
2009-03-30 19:14:04 +0000

    initial translation

--- jquantlib/src/main/java/org/jquantlib/math/optimization/SteepestDescent.java
+++ jquantlib/src/main/java/org/jquantlib/math/optimization/SteepestDescent.java
@@ -21,20 +21,83 @@
  */
 package org.jquantlib.math.optimization;
 
+import org.jquantlib.math.Array;
 import org.jquantlib.math.optimization.EndCriteria.CriteriaType;
 
 public class SteepestDescent extends LineSearchBasedMethod {
     
-    public SteepestDescent(){
+    public SteepestDescent(LineSearch lineSearch){
         if (System.getProperty("EXPERIMENTAL") == null) {
             throw new UnsupportedOperationException("Work in progress");
         }
+        if(lineSearch == null){
+            lineSearch = new LineSearch();
+        }
+        
     }
 
     @Override
     public CriteriaType minimize(Problem P, EndCriteria endCriteria) {
-        // TODO Auto-generated method stub
-        return null;
+       EndCriteria.CriteriaType ecType = EndCriteria.CriteriaType.None;
+       P.reset();
+       Array x_ = P.currentValue();
+       int iterationNumber = 0;
+       int stationaryStateIterationNumber_ = 0;
+       lineSearch_.setSearchDirection(new Array(x_.size()));
+       boolean end = false;
+       
+       // function and squared norm of gradient values
+       double normdiff;
+       // classical initial value for line-search step
+       double t = 1.0;
+       // set gold at the size of the optimization problem search direction
+       Array gold = new Array(lineSearch_.searchDirection().size());
+       Array gdiff = new Array(lineSearch_.searchDirection().size());
+       
+       P.setFunctionValue(P.valueAndGradient(gold, x_));
+       lineSearch_.searchDirection_ = gold.operatorMultiplyCopy(-1);
+       P.setGradientNormValue(Array.dotProduct(gold, gold));
+       normdiff = Math.sqrt(P.gradientNormValue());
+       
+       do{
+           // Linesearch
+           t = lineSearch_.evaluate(P, ecType, endCriteria, t);
+           if(lineSearch_.succeed_ == false){
+               throw new ArithmeticException("line search failed");
+           }
+           // End
+           end = endCriteria.bracket_operator(iterationNumber, 
+                   stationaryStateIterationNumber_, 
+                   true, //FIXME: it should be in the problem
+                   P.functionValue(),
+                   Math.sqrt(P.gradientNormValue()),
+                   lineSearch_.lastFunctionValue(),
+                   Math.sqrt(lineSearch_.lastGradientNormNorm2()), 
+                   ecType
+                   //FIXME: it's never been used! ???
+                   // , normdiff
+                   );
+           
+           // Updates
+           // New point
+           x_ = lineSearch_.lastX();
+           // New function value
+           P.setFunctionValue(lineSearch_.lastFunctionValue());
+           // New gradient and search direction vectors
+           gdiff = gold.operatorSubtractCopy(lineSearch_.lastGradient());
+           normdiff = Math.sqrt(Array.dotProduct(gdiff, gdiff));
+           gold = lineSearch_.lastGradient();
+           lineSearch_.setSearchDirection(gold.operatorMultiplyCopy(-1));
+           // New gradient squared norm
+           P.setGradientNormValue(lineSearch_.lastGradientNormNorm2());
+       
+           // Increase iteration number
+           ++iterationNumber; 
+       }
+       
+       while(end == false);
+       
+       P.setCurrentValue(x_);
+       return ecType;
     }
-    
 }
