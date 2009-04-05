290c4c27ca588db2bbf1384acb403f948fbade21
hofi85 <hofi85@945d5b45-6d3a-0410-8913-86594e9677e0>
2009-04-05 23:51:46 +0000

    initial import rng tests - from cpp rngtraits

---
+++ jquantlib/src/test/java/org/jquantlib/testsuite/math/randomnumbers/RandomNumberTest.java
@@ -0,0 +1,36 @@
+package org.jquantlib.testsuite.math.randomnumbers;
+
+import org.jquantlib.math.optimization.NoConstraint;
+import org.jquantlib.math.randomnumbers.PseudoRandom;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RandomNumberTest {
+    
+    private final static Logger logger = LoggerFactory.getLogger(RandomNumberTest.class);
+    
+    public RandomNumberTest() {
+        logger.info("\n\n::::: "+this.getClass().getSimpleName()+" :::::");
+    }
+    
+    @Test
+    public void testGaussian(){
+        logger.info("Testing Gaussian pseudo-random number generation...");
+        logger.info("to be implemented");
+
+    }
+    
+    @Test
+    public void testDefaultPoisson(){
+        logger.info("Testing Poisson pseudo-random number generation...");
+        logger.info("to be implemented");
+    }
+    
+    @Test
+    public void testCustomPoisson(){
+        logger.info("Testing custom Poisson pseudo-random number generation...");
+        logger.info("to be implemented");
+    }
+
+}
