e8caaca6ba09aaf716e793acc836d19df185edcd
hofi85 <hofi85@945d5b45-6d3a-0410-8913-86594e9677e0>
2009-04-11 22:21:54 +0000

    reviewed

--- jquantlib/src/main/java/org/jquantlib/model/CalibrationHelper.java
+++ jquantlib/src/main/java/org/jquantlib/model/CalibrationHelper.java
@@ -23,6 +23,8 @@ package org.jquantlib.model;
 
 import java.util.ArrayList;
 import org.jquantlib.lang.annotation.Time;
+import org.jquantlib.math.UnaryFunctionDouble;
+import org.jquantlib.math.solvers1D.Brent;
 import org.jquantlib.pricingengines.PricingEngine;
 import org.jquantlib.quotes.Handle;
 import org.jquantlib.quotes.Quote;
@@ -34,36 +36,39 @@ import org.jquantlib.util.Observer;
  * 
  * @author Praneet Tiwari
  */
+//! liquid market instrument used during calibration
+
+/********** CLASS REVIEWED/REFACTORED ONCE ********************/
 public abstract class CalibrationHelper implements Observer, Observable {
 
     protected double/* @Real */marketValue;
-    protected Handle<Quote> volatility;
-    protected Handle<YieldTermStructure> termStructure;
-    protected PricingEngine engine;
-    private boolean calibrateVolatility = false;
+    protected Handle<Quote> volatility_;
+    protected Handle<YieldTermStructure> termStructure_;
+    protected PricingEngine engine_;
+    private boolean calibrateVolatility_ = false;
 
     // private:
     // class ImpliedVolatilityHelper;
-
+    
+    
+    //reviewed
     public CalibrationHelper(final Handle<Quote> volatility, final Handle<YieldTermStructure> termStructure,
             boolean calibrateVolatility) {
         if (System.getProperty("EXPERIMENTAL") == null) {
             throw new UnsupportedOperationException("Work in progress");
         }
-        this.volatility = volatility;
-        this.termStructure = termStructure;
-        this.calibrateVolatility = calibrateVolatility;
-        /*
-         * registerWith(volatility_); registerWith(termStructure_);
-         */
-
+        this.volatility_ = volatility;
+        this.termStructure_ = termStructure;
+        this.calibrateVolatility_ = calibrateVolatility;
+        
+        termStructure_.addObserver(this);
+        volatility_.addObserver(this);
     }
 
     // abstract double blackPrice (double /*@Volatility*/ volatility);
 
     public void update() {
-        /* marketValue_ = blackPrice(volatility_->value()); */
-        marketValue = blackPrice(volatility.getLink().evaluate());
+        marketValue = blackPrice(volatility_.getLink().evaluate());
         notifyObservers();
     }
 
@@ -76,18 +81,62 @@ public abstract class CalibrationHelper implements Observer, Observable {
     public abstract double /* @Real */modelValue();
 
     // ! returns the error resulting from the model valuation
-    public abstract double /* @Real */calibrationError();
+    public double /* @Real */calibrationError(){
+        if(calibrateVolatility_){
+            double lowerPrice = blackPrice(0.001);
+            double upperPrice = blackPrice(10);
+            double modelPrice = modelValue();
+            
+            double implied;
+            if(modelPrice <= lowerPrice){
+                implied = 0.001;
+            }
+            else{
+                if(modelPrice >= upperPrice){
+                    implied = 10.0;
+                }
+                else{
+                    implied = impliedVolatility(modelPrice, 1e-12, 5000, 0.001, 10);
+                }
+            }
+            return implied - volatility_.getLink().evaluate();
+        }
+        else{
+            return Math.abs(marketValue() - modelValue())/marketValue();
+        }
+        
+    }
 
     public abstract void addTimesTo(ArrayList<Time> times);
 
     // ! Black volatility implied by the model
-    public abstract double /* @Volatility */impliedVolatility(double /* @Real */targetValue, double /* @Real */accuracy,
-            int /* @Size */maxEvaluations, double /* @Volatility */minVol, double /* @Volatility */maxVol);
+    public double /* @Volatility */impliedVolatility(double /* @Real */targetValue, double /* @Real */accuracy,
+            int /* @Size */maxEvaluations, double /* @Volatility */minVol, double /* @Volatility */maxVol){
+        ImpliedVolatilityHelper f = new ImpliedVolatilityHelper(this, targetValue);
+        Brent solver = new Brent();
+        solver.setMaxEvaluations(maxEvaluations);
+        return solver.solve(f, accuracy, volatility_.getLink().evaluate(), minVol, maxVol);
+    }
 
     // ! Black price given a volatility
     public abstract double /* @Real */blackPrice(double /* @Volatility */volatility);
 
     public void setPricingEngine(final PricingEngine engine) {
-        this.engine = engine;
+        this.engine_ = engine;
+    }
+    
+    private class ImpliedVolatilityHelper implements UnaryFunctionDouble{
+        public ImpliedVolatilityHelper(CalibrationHelper helper, double value){
+            this.helper_ = helper;
+            this.value_ = value;
+        }
+        
+        public double evaluate(final double x){
+            return value_ - helper_.blackPrice(x);
+        }
+        
+        private CalibrationHelper helper_;
+        private double value_;
+        
     }
 }
