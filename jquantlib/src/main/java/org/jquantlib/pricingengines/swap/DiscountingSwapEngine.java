83581787748035c4c7eae23b62e1e82da6e40619
hofi85 <hofi85@945d5b45-6d3a-0410-8913-86594e9677e0>
2009-07-22 23:39:20 +0000

    stub

---
+++ jquantlib/src/main/java/org/jquantlib/pricingengines/swap/DiscountingSwapEngine.java
@@ -0,0 +1,17 @@
+package org.jquantlib.pricingengines.swap;
+
+import org.jquantlib.instruments.Swap;
+import org.jquantlib.instruments.Swap.Arguments;
+import org.jquantlib.instruments.Swap.Results;
+import org.jquantlib.quotes.Handle;
+import org.jquantlib.termstructures.YieldTermStructure;
+
+public class DiscountingSwapEngine extends Swap.Engine {
+    
+        public DiscountingSwapEngine(Swap swap, Arguments arguments, Results results) {
+        swap.super(arguments, results);
+        // TODO Auto-generated constructor stub
+        }
+
+        private Handle<YieldTermStructure> discountCurve_;
+}
