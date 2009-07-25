07dffd1c11d8bca52414f2f358214bd37b3bcf26
hofi85 <hofi85@945d5b45-6d3a-0410-8913-86594e9677e0>
2009-07-25 00:23:29 +0000

    switched from list<double> to array --> erasure problem resolved

--- jquantlib/src/main/java/org/jquantlib/cashflow/FixedRateLeg.java
+++ jquantlib/src/main/java/org/jquantlib/cashflow/FixedRateLeg.java
@@ -18,8 +18,10 @@ import static org.jquantlib.Error.*;
 public class FixedRateLeg {
     
 private Schedule schedule_;
-private List</*Real*/Double> notionals_;
-private List<InterestRate> couponRates_;
+//private List</*Real*/Double> notionals_;
+private double[] notionals_;
+//private List<InterestRate> couponRates_;
+private InterestRate[] couponRates_;
 private  DayCounter paymentDayCounter_, firstPeriodDayCounter_;
 private  BusinessDayConvention paymentAdjustment_;
     
@@ -30,37 +32,36 @@ public FixedRateLeg(final Schedule schedule, final DayCounter paymentDayCounter)
 }
 
     public FixedRateLeg withNotationals(/* Real */double notional) {
-        this.notionals_ = Arrays.asList(new Double[] { notional });
+        this.notionals_ = new double[] {notional};
         return this;
     }
 
-    public FixedRateLeg withNotionals(final List<Double> notionals) {
+    public FixedRateLeg withNotionals(final double[]/*List<Double>*/ notionals) {
         this.notionals_ = notionals;
         return this;
     }
 
     public FixedRateLeg withCouponRates(/* @Rate */double couponRate) {
-        couponRates_.clear();
-        couponRates_.set(0, new InterestRate(couponRate, paymentDayCounter_, Compounding.SIMPLE));
+        couponRates_ = new InterestRate[]{new InterestRate(couponRate, paymentDayCounter_, Compounding.SIMPLE)};
+//        couponRates_.clear();
+//        couponRates_.set(0, new InterestRate(couponRate, paymentDayCounter_, Compounding.SIMPLE));
         return this;
     }
 
     public FixedRateLeg withCouponRates(final InterestRate couponRate) {
-
-        this.couponRates_.set(0, couponRate);
+        couponRates_ = new InterestRate[]{couponRate};
         return this;
     }
     
-    public FixedRateLeg withCouponRates(/* @Rate */List<Double> couponRates) {
-        couponRates_.clear();
-        for(int i = 0; i<couponRates.size(); i++){
-            couponRates_.set(i, new InterestRate(couponRates.get(i), paymentDayCounter_, Compounding.SIMPLE));
+    public FixedRateLeg withCouponRates(/* @Rate */double [] couponRates) {
+        couponRates_ = new InterestRate[couponRates.length];
+        for(int i = 0; i<couponRates.length; i++){
+            couponRates_[i] = new InterestRate(couponRates[i], paymentDayCounter_, Compounding.SIMPLE);
         }
         return this;
     }
     
-    //FIXME: erasure problem .. damn generics..
-    public FixedRateLeg withCouponRates_(final List<InterestRate> couponRates) {
+    public FixedRateLeg withCouponRates(final InterestRate [] couponRates) {
         couponRates_ = couponRates;
         return this;
     }
@@ -77,8 +78,8 @@ public FixedRateLeg(final Schedule schedule, final DayCounter paymentDayCounter)
     
     
     public List<CashFlow> Leg() {
-        QL_REQUIRE(!couponRates_.isEmpty(), "coupon rates not specified");
-        QL_REQUIRE(!notionals_.isEmpty(), "nominals not specified");
+        QL_REQUIRE(!(couponRates_  == null) && !(couponRates_.length == 0), "coupon rates not specified");
+        QL_REQUIRE(!(notionals_  == null) && !(notionals_.length == 0), "nominals not specified");
 
         //Leg leg;
         List<CashFlow> leg = new ArrayList<CashFlow>();
@@ -89,8 +90,8 @@ public FixedRateLeg(final Schedule schedule, final DayCounter paymentDayCounter)
         // first period might be short or long
         Date start = schedule_.date(0), end = schedule_.date(1);
         Date paymentDate = calendar.adjust(end, paymentAdjustment_);
-        InterestRate rate = couponRates_.get(0);
-        /*@Real*/ double nominal = notionals_.get(0);
+        InterestRate rate = couponRates_[0];
+        /*@Real*/ double nominal = notionals_[0];
         if (schedule_.isRegular(1)) {
             //FIXME: empty() method on dayCounter
             QL_REQUIRE(firstPeriodDayCounter_/*.empty()*/==null ||
@@ -111,17 +112,17 @@ public FixedRateLeg(final Schedule schedule, final DayCounter paymentDayCounter)
         for (int i=2; i<schedule_.size()-1; ++i) {
             start = end; end = schedule_.date(i);
             paymentDate = calendar.adjust(end, paymentAdjustment_);
-            if ((i-1) < couponRates_.size()){
-                rate = couponRates_.get(i-1);
+            if ((i-1) < couponRates_.length){
+                rate = couponRates_[i-1];
             }
             else{
-                rate = couponRates_.get(couponRates_.size() - 1);
+                rate = couponRates_[couponRates_.length - 1];
             }
-            if ((i-1) < notionals_.size()){
-                nominal = notionals_.get(i-1);
+            if ((i-1) < notionals_.length){
+                nominal = notionals_[i-1];
             }
             else{
-                nominal = notionals_.get(notionals_.size()-1);
+                nominal = notionals_[notionals_.length-1];
             }
             leg.add(new FixedRateCoupon(nominal, paymentDate,
                                                     rate, paymentDayCounter_,
@@ -132,17 +133,17 @@ public FixedRateLeg(final Schedule schedule, final DayCounter paymentDayCounter)
             int N = schedule_.size();
             start = end; end = schedule_.date(N-1);
             paymentDate = calendar.adjust(end, paymentAdjustment_);
-            if ((N-2) < couponRates_.size()){
-                rate = couponRates_.get(N-2);
+            if ((N-2) < couponRates_.length){
+                rate = couponRates_[N-2];
             }
             else{
-                rate = couponRates_.get(couponRates_.size() - 1);
+                rate = couponRates_[couponRates_.length - 1];
             }
-            if ((N-2) < notionals_.size()){
-                nominal = notionals_.get(N-2);
+            if ((N-2) < notionals_.length){
+                nominal = notionals_[N-2];
             }
             else{
-                nominal = notionals_.get(notionals_.size()-1);
+                nominal = notionals_[notionals_.length-1];
             }
             if (schedule_.isRegular(N-1)) {
                 leg.add(new FixedRateCoupon(nominal, paymentDate,
