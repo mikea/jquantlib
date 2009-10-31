ba76db1b63f62453cb68cb68bf9ae3a52082beec
fgomes <fgomes@945d5b45-6d3a-0410-8913-86594e9677e0>
2009-10-31 22:45:53 +0000

    Resolving implementation of PricerSetter http://bugs.jquantlib.org/view.php?id=323

--- jquantlib/src/main/java/org/jquantlib/cashflow/PricerSetter.java
+++ jquantlib/src/main/java/org/jquantlib/cashflow/PricerSetter.java
@@ -3,16 +3,19 @@ package org.jquantlib.cashflow;
 import java.util.List;
 
 import org.jquantlib.QL;
+import org.jquantlib.lang.annotation.QualityAssurance;
+import org.jquantlib.lang.annotation.QualityAssurance.Quality;
+import org.jquantlib.lang.annotation.QualityAssurance.Version;
 import org.jquantlib.lang.exceptions.LibraryException;
 import org.jquantlib.util.TypedVisitor;
 import org.jquantlib.util.Visitor;
 
 /**
+ * Coupon selector to PricerSetter
  *
  * @author Richard Gomes
  */
-// TODO: code review :: Please review this class! :S
-// TODO: code review :: license, class comments, comments for access modifiers, comments for @Override
+@QualityAssurance(quality=Quality.Q0_UNFINISHED, version=Version.V097, reviewers="Richard Gomes")
 public class PricerSetter implements TypedVisitor<Object> {
 
     private static final String INCOMPATIBLE_PRICER = "incompatible pricer";
@@ -73,20 +76,20 @@ public class PricerSetter implements TypedVisitor<Object> {
             return new IborCouponVisitor();
         }
 
-        //        if (klass == CmsCoupon.class)
-        //            return new CmsCouponVisitor();
-        //        if (klass == CappedFlooredIborCoupon.class)
-        //            return new CappedFlooredIborCouponVisitor();
-        //        if (klass == CappedFlooredCmsCoupon.class)
-        //            return new CappedFlooredCmsCouponVisitor();
-        //        if (klass == DigitalIborCoupon.class)
-        //            return new DigitalIborCouponVisitor();
-        //        if (klass == DigitalCmsCoupon.class)
-        //            return new DigitalCmsCouponVisitor();
-        //        if (klass == RangeAccrualFloatersCoupon.class)
-        //            return new RangeAccrualFloatersCouponVisitor();
-        //        if (klass == SubPeriodsCoupon.class)
-        //            return new SubPeriodsCouponVisitor();
+//        if (klass == CmsCoupon.class)
+//            return new CmsCouponVisitor();
+//        if (klass == CappedFlooredIborCoupon.class)
+//            return new CappedFlooredIborCouponVisitor();
+//        if (klass == CappedFlooredCmsCoupon.class)
+//            return new CappedFlooredCmsCouponVisitor();
+//        if (klass == DigitalIborCoupon.class)
+//            return new DigitalIborCouponVisitor();
+//        if (klass == DigitalCmsCoupon.class)
+//            return new DigitalCmsCouponVisitor();
+//        if (klass == RangeAccrualFloatersCoupon.class)
+//            return new RangeAccrualFloatersCouponVisitor();
+//        if (klass == SubPeriodsCoupon.class)
+//            return new SubPeriodsCouponVisitor();
 
 
         throw new LibraryException(UNKNOWN_VISITABLE); // QA:[RG]::verified
@@ -118,93 +121,93 @@ public class PricerSetter implements TypedVisitor<Object> {
                 final IborCoupon c = (IborCoupon) o;
                 c.setPricer(pricer);
             } else {
-                QL.require(false , INCOMPATIBLE_PRICER);
+                throw new LibraryException(INCOMPATIBLE_PRICER); // QA:[RG]::verified
             }
         }
     }
 
-    //    private class CmsCouponVisitor implements Visitor<Object> {
-    //        @Override
-    //        public void visit(Object o) {
-    //            if (CmsCouponPricer.class.isAssignableFrom(pricer.getClass())) {
-    //                final CmsCoupon c = (CmsCoupon) o;
-    //                c.setPricer((CmsCouponPricer)pricer);
-    //            } else {
-    //                Exceptions.fail(INCOMPATIBLE_PRICER, pricer.getClass().getSimpleName());
-    //            }
-    //        }
-    //    }
-    //
-    //    private class CappedFlooredIborCouponVisitor implements Visitor<Object> {
-    //        @Override
-    //        public void visit(Object o) {
-    //            if (CappedFlooredIborCouponPricer.class.isAssignableFrom(pricer.getClass())) {
-    //                final CappedFlooredIborCoupon c = (CappedFlooredIborCoupon) o;
-    //                c.setPricer((CappedFlooredIborCouponPricer)pricer);
-    //            } else {
-    //                Exceptions.fail(INCOMPATIBLE_PRICER, pricer.getClass().getSimpleName());
-    //            }
-    //        }
-    //    }
-    //
-    //    private class CappedFlooredCmsCouponVisitor implements Visitor<Object> {
-    //        @Override
-    //        public void visit(Object o) {
-    //            if (CappedFlooredCmsCouponPricer.class.isAssignableFrom(pricer.getClass())) {
-    //                final CappedFlooredCmsCoupon c = (CappedFlooredCmsCoupon) o;
-    //                c.setPricer((CappedFlooredCmsCouponPricer)pricer);
-    //            } else {
-    //                Exceptions.fail(INCOMPATIBLE_PRICER, pricer.getClass().getSimpleName());
-    //            }
-    //        }
-    //    }
-    //
-    //    private class DigitalIborCouponVisitor implements Visitor<Object> {
-    //        @Override
-    //        public void visit(Object o) {
-    //            if (DigitalIborCouponPricer.class.isAssignableFrom(pricer.getClass())) {
-    //                final DigitalIborCoupon c = (DigitalIborCoupon) o;
-    //                c.setPricer((DigitalIborCouponPricer)pricer);
-    //            } else {
-    //                Exceptions.fail(INCOMPATIBLE_PRICER, pricer.getClass().getSimpleName());
-    //            }
-    //        }
-    //    }
-    //
-    //    private class DigitalCmsCouponVisitor implements Visitor<Object> {
-    //        @Override
-    //        public void visit(Object o) {
-    //            if (DigitalCmsCouponPricer.class.isAssignableFrom(pricer.getClass())) {
-    //                final DigitalCmsCoupon c = (DigitalCmsCoupon) o;
-    //                c.setPricer((DigitalCmsCouponPricer)pricer);
-    //            } else {
-    //                Exceptions.fail(INCOMPATIBLE_PRICER, pricer.getClass().getSimpleName());
-    //            }
-    //        }
-    //    }
-    //
-    //    private class RangeAccrualFloatersCouponVisitor implements Visitor<Object> {
-    //        @Override
-    //        public void visit(Object o) {
-    //            if (RangeAccrualFloatersCouponPricer.class.isAssignableFrom(pricer.getClass())) {
-    //                final RangeAccrualFloatersCoupon c = (RangeAccrualFloatersCoupon) o;
-    //                c.setPricer((RangeAccrualFloatersCouponPricer)pricer);
-    //            } else {
-    //                Exceptions.fail(INCOMPATIBLE_PRICER, pricer.getClass().getSimpleName());
-    //            }
-    //        }
-    //    }
-    //
-    //    private class SubPeriodsCouponVisitor implements Visitor<Object> {
-    //        @Override
-    //        public void visit(Object o) {
-    //            if (SubPeriodsCouponPricer.class.isAssignableFrom(pricer.getClass())) {
-    //                final SubPeriodsCoupon c = (SubPeriodsCoupon) o;
-    //                c.setPricer((SubPeriodsCouponPricer)pricer);
-    //            } else {
-    //                Exceptions.fail(INCOMPATIBLE_PRICER, pricer.getClass().getSimpleName());
-    //            }
-    //        }
-    //    }
+//    private class CmsCouponVisitor implements Visitor<Object> {
+//        @Override
+//        public void visit(final Object o) {
+//            if (CmsCouponPricer.class.isAssignableFrom(pricer.getClass())) {
+//                final CmsCoupon c = (CmsCoupon) o;
+//                c.setPricer((CmsCouponPricer)pricer);
+//            } else {
+//                throw new LibraryException(INCOMPATIBLE_PRICER); // QA:[RG]::verified
+//            }
+//        }
+//    }
+//
+//    private class CappedFlooredIborCouponVisitor implements Visitor<Object> {
+//        @Override
+//        public void visit(final Object o) {
+//            if (CappedFlooredIborCouponPricer.class.isAssignableFrom(pricer.getClass())) {
+//                final CappedFlooredIborCoupon c = (CappedFlooredIborCoupon) o;
+//                c.setPricer((CappedFlooredIborCouponPricer)pricer);
+//            } else {
+//                throw new LibraryException(INCOMPATIBLE_PRICER); // QA:[RG]::verified
+//            }
+//        }
+//    }
+//
+//    private class CappedFlooredCmsCouponVisitor implements Visitor<Object> {
+//        @Override
+//        public void visit(final Object o) {
+//            if (CappedFlooredCmsCouponPricer.class.isAssignableFrom(pricer.getClass())) {
+//                final CappedFlooredCmsCoupon c = (CappedFlooredCmsCoupon) o;
+//                c.setPricer((CappedFlooredCmsCouponPricer)pricer);
+//            } else {
+//                throw new LibraryException(INCOMPATIBLE_PRICER); // QA:[RG]::verified
+//            }
+//        }
+//    }
+//
+//    private class DigitalIborCouponVisitor implements Visitor<Object> {
+//        @Override
+//        public void visit(final Object o) {
+//            if (DigitalIborCouponPricer.class.isAssignableFrom(pricer.getClass())) {
+//                final DigitalIborCoupon c = (DigitalIborCoupon) o;
+//                c.setPricer((DigitalIborCouponPricer)pricer);
+//            } else {
+//                throw new LibraryException(INCOMPATIBLE_PRICER); // QA:[RG]::verified
+//            }
+//        }
+//    }
+//
+//    private class DigitalCmsCouponVisitor implements Visitor<Object> {
+//        @Override
+//        public void visit(final Object o) {
+//            if (DigitalCmsCouponPricer.class.isAssignableFrom(pricer.getClass())) {
+//                final DigitalCmsCoupon c = (DigitalCmsCoupon) o;
+//                c.setPricer((DigitalCmsCouponPricer)pricer);
+//            } else {
+//                throw new LibraryException(INCOMPATIBLE_PRICER); // QA:[RG]::verified
+//            }
+//        }
+//    }
+//
+//    private class RangeAccrualFloatersCouponVisitor implements Visitor<Object> {
+//        @Override
+//        public void visit(final Object o) {
+//            if (RangeAccrualFloatersCouponPricer.class.isAssignableFrom(pricer.getClass())) {
+//                final RangeAccrualFloatersCoupon c = (RangeAccrualFloatersCoupon) o;
+//                c.setPricer((RangeAccrualFloatersCouponPricer)pricer);
+//            } else {
+//                throw new LibraryException(INCOMPATIBLE_PRICER); // QA:[RG]::verified
+//            }
+//        }
+//    }
+//
+//    private class SubPeriodsCouponVisitor implements Visitor<Object> {
+//        @Override
+//        public void visit(final Object o) {
+//            if (SubPeriodsCouponPricer.class.isAssignableFrom(pricer.getClass())) {
+//                final SubPeriodsCoupon c = (SubPeriodsCoupon) o;
+//                c.setPricer((SubPeriodsCouponPricer)pricer);
+//            } else {
+//                throw new LibraryException(INCOMPATIBLE_PRICER); // QA:[RG]::verified
+//            }
+//        }
+//    }
 
 }
