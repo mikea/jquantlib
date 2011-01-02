a79a0cb29ea25a87d3d98e9180a2c2e26889bd10
fgomes <fgomes@945d5b45-6d3a-0410-8913-86594e9677e0>
2011-01-02 19:05:46 +0000

    Reorganization of project build (see jquantlib-all, which builds jquantilb-parent)

--- jquantlib-samples/src/test/java/org/jquantlib/samples/TestSamples.java
+++ jquantlib-samples/src/test/java/org/jquantlib/samples/TestSamples.java
@@ -1,73 +1,30 @@
 package org.jquantlib.samples;
 
-import java.util.Arrays;
-
-import org.jquantlib.lang.iterators.Iterables;
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestSamples {
 
-    private static final Class<?> complete[] = {
-        Calendars.class, 
-        ConvertibleBonds.class, 
-        Dates.class, 
-        VolatilityTermStructures.class, 
-    };
-
-    private static final Class<?> incomplete[] = {
-        EquityOptions.class, 
-    };
-
+    private final AllSamples samples;
     
-    private static final Class<?> pending[] = {
-        Bonds.class, 
-        YieldCurveTermStructures.class, 
-        BermudanSwaption.class, 
-        FRA.class, 
-        Processes.class, 
-        Replication.class, 
-        Repo.class, 
-        Swap.class, 
-        DiscreteHedging.class, 
-        CoxRossWithHullWhite.class, 
-        SobolChartSample.class
-    };
+    public TestSamples() {
+        this.samples = new AllSamples();
+    }
     
-
     @Test
     public void testCompleteSamples() {
-        testSamples(complete);
+        samples.runCompleteSamples();
     }
     
     @Test
     public void testIncompleteSamples() {
-        testSamples(incomplete);
+        samples.runIncompleteSamples();
     }
     
     @Ignore
     @Test
     public void testPendingSamples() {
-        testSamples(complete);
+        samples.runPendingSamples();
     }
-    
-    
-    private void testSamples(final Class<?> klasses[]) {
-        for (final Class<?> klass : Iterables.unmodifiableIterable(Arrays.asList(klasses).iterator())) {
-            System.err.println();
-            System.err.println("*************************************************************");
-            System.err.println("* Running " + klass.getSimpleName());
-            System.err.println("*************************************************************");
-            Runnable r = null;
-            try {
-                r = (Runnable) klass.newInstance();
-                r.run();
-            } catch (final Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-    
-    
 
 }
