ef996ec792aeb642f8c66115e07a31e81c27827c
neelsheyal <neelsheyal@945d5b45-6d3a-0410-8913-86594e9677e0>
2011-01-11 21:28:12 +0000

    Mantis Issue: 481
    + Functionally, the code is now compatible with QL. 
    + We need to fix the double[] issue. However, it will trigger a chain reaction..(at least FixedRateBond and FixedRateLeg will get impacted and everything that depends on them).

--- jquantlib/src/main/java/org/jquantlib/termstructures/yieldcurves/FixedRateBondHelper.java
+++ jquantlib/src/main/java/org/jquantlib/termstructures/yieldcurves/FixedRateBondHelper.java
@@ -1,6 +1,6 @@
 /*
  Copyright (C) 2008 Srinivas Hasti
-
+ Copyright (C) 2010 Neel Sheyal
  This source code is release under the BSD License.
  
  This file is part of JQuantLib, a free-software/open-source library
@@ -25,50 +25,170 @@ package org.jquantlib.termstructures.yieldcurves;
 import java.util.List;
 
 import org.jquantlib.QL;
+import org.jquantlib.Settings;
 import org.jquantlib.daycounters.DayCounter;
+import org.jquantlib.instruments.bonds.FixedRateBond;
+import org.jquantlib.pricingengines.PricingEngine;
+import org.jquantlib.pricingengines.bond.DiscountingBondEngine;
 import org.jquantlib.quotes.Handle;
 import org.jquantlib.quotes.Quote;
+import org.jquantlib.quotes.RelinkableHandle;
+import org.jquantlib.termstructures.BootstrapHelper;
 import org.jquantlib.termstructures.RateHelper;
+import org.jquantlib.termstructures.TermStructure;
+import org.jquantlib.termstructures.YieldTermStructure;
 import org.jquantlib.time.BusinessDayConvention;
 import org.jquantlib.time.Date;
 import org.jquantlib.time.Schedule;
+import org.jquantlib.util.TypedVisitor;
+import org.jquantlib.util.Visitor;
 
-//TODO: This class still needs to be finished
-public class FixedRateBondHelper<YieldTermStructure> extends RateHelper {
+/**
+ * fixed-coupon bond helper
+ * 
+ * {@literal WARNING:This class assumes that the reference date does not change
+ * between calls of setTermStructure()}
+ * 
+ * @category instruments
+ * 
+ */
+//
+//
+// COMMENTS::
+// 1. In QL, the variable coupons is passed as a std::vector. Should we be pass a List? Then FixedRateBond needs a new ctor
+//
+//
+public class FixedRateBondHelper extends RateHelper {
 
-	public FixedRateBondHelper(
-            Handle<Quote> cleanPrice,
-            int settlementDays,
-            double faceAmount,
-            Schedule schedule,
-            List<Double> coupons,
-            DayCounter dayCounter,
-            BusinessDayConvention paymentConvention,
-            double redemption,
-            Date issueDate)
- {
-     super (cleanPrice);
-     QL.validateExperimentalMode();
-     
-	 //super(cleanPrice);
-	 /*
-bond_ = boost::shared_ptr<FixedRateBond>(new
-FixedRateBond(settlementDays, faceAmount, schedule,
-  coupons, dayCounter, paymentConvention,
-  redemption, issueDate));
-
-latestDate_ = bond_->maturityDate();
-registerWith(Settings::instance().evaluationDate());
-
-boost::shared_ptr<PricingEngine> bondEngine(new
-DiscountingBondEngine(termStructureHandle_));
-bond_->setPricingEngine(bondEngine); */
+	//
+	// private fields
+	//
+     private FixedRateBond bond;
+     private final RelinkableHandle<YieldTermStructure> termStructureHandle = new RelinkableHandle<YieldTermStructure>(null);
+    
+	/**
+	 * 
+	 * @param cleanPrice
+	 * @param settlementDays
+	 * @param faceAmount
+	 * @param schedule
+	 * @param coupons
+	 * @param dayCounter
+	 * @param paymentConvention: default value = BusinessDayConvention.Following
+	 * @param redemption: default value = 100 
+	 * @param issueDate: default: new Date()
+	 */
+	public FixedRateBondHelper(final Handle<Quote> cleanPrice,
+                                final /* Natural */ int settlementDays,
+                                final /* Real */ double faceAmount,
+                                final Schedule schedule,
+                                final /* Rate */ double[] coupons,
+                                final DayCounter dayCounter) {
+		this(cleanPrice, settlementDays, faceAmount, schedule, coupons,
+				dayCounter, BusinessDayConvention.Following, 100.0, new Date());
+	}
+	
+	/**
+	 * 
+	 * @param cleanPrice
+	 * @param settlementDays
+	 * @param faceAmount
+	 * @param schedule
+	 * @param coupons
+	 * @param dayCounter
+	 * @param paymentConvention
+	 * @param redemption
+	 * @param issueDate
+	 */
+	public FixedRateBondHelper(final Handle<Quote> cleanPrice,
+                                final /* Natural */ int settlementDays,
+                                final /* Real */ double faceAmount,
+                                final Schedule schedule,
+                                final /* Rate */ double[] coupons, 
+                                final DayCounter dayCounter,
+                                final BusinessDayConvention paymentConvention,
+                                final /* Real */ double redemption,
+                                final Date issueDate) {
+		super(cleanPrice);
+		QL.validateExperimentalMode();  
+		
+		this.bond = new FixedRateBond(settlementDays, faceAmount, schedule,
+				coupons, dayCounter, paymentConvention, redemption, issueDate);
+		
+		this.latestDate = this.bond.maturityDate();
+		new Settings().evaluationDate().addObserver(this);
+		
+		PricingEngine bondEngine = new DiscountingBondEngine(this.termStructureHandle);
+		this.bond.setPricingEngine(bondEngine);
+			 		
 }
-            
+	
+	/**
+	 *  {@literal WARNING: Setting a pricing engine to the passed bond from
+                     external code will cause the bootstrap to fail or
+                     to give wrong results. It is advised to discard
+                     the bond after creating the helper, so that the
+                     helper has sole ownership of it.}
+     *                
+	 * @param cleanPrice
+	 * @param bond
+	 */
+	public FixedRateBondHelper(final Handle<Quote> cleanPrice,
+			                    final FixedRateBond bond) {
+		super(cleanPrice);
+		QL.validateExperimentalMode();  
+		
+		this.latestDate = bond.maturityDate();
+		new Settings().evaluationDate().addObserver(this);
+		
+		PricingEngine bondEngine = new DiscountingBondEngine(this.termStructureHandle);
+		this.bond.setPricingEngine(bondEngine);
+			
+	}
+	
+	//
+	// public methods
+	//
+	/*
+     *  * @see org.jquantlib.termstructures.BootstrapHelper.setTermStructure(TS ts)
+     * 
+     */
 	@Override
-	public double impliedQuote() {
-		// TODO Auto-generated method stub
-		return 0;
+	public void  setTermStructure(YieldTermStructure t) {
+		// do not set the relinkable handle as an observer -
+        // force recalculation when needed
+		this.termStructureHandle.linkTo(t,false);
+		super.setTermStructure(t);
+    }
+    
+    
+	public FixedRateBond  bond() {
+    	return this.bond;
+    }
+
+	/**
+	 * * @see org.jquantlib.termstructures.BootstrapHelper.impliedQuote()
+	 * 
+	 */
+	@Override
+	public/* Real */double impliedQuote() {
+		QL.require(this.termStructure != null, "term structure not set");
+		this.bond.recalculate();
+		return this.bond.cleanPrice();
+	}
+    
+	//
+	// implements TypedVisitable
+	//
+	@Override
+	public void accept(final TypedVisitor<BootstrapHelper> v) {
+		final Visitor<BootstrapHelper> v1 = (v != null) ? v.getVisitor(this
+				.getClass()) : null;
+		if (v1 != null) {
+			v1.visit(this);
+		} else {
+			super.accept(v);
+		}
 	}
 
 }
